7eb1394f46ccbb54379e05ba44a24cde5db76aa1
Dustin Campbell <dustin@teamcampbell.org>
2016-04-14 13:34:40 -0700

    Update with latest OmniSharp and pick correct version for current Linux distribution
    We've updated OmniSharp to build releases for each Linux distribution that we currently support: CentOS, Debian, Red Hat (RHEL), and Ubuntu. This change adds code to sniff out which of the supported Linux distros we're running on to determine which flavor of OmniSharp we need to download and install.

---
+++ src/utils.ts
@@ -0,0 +1,46 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import * as child_process from 'child_process'
+
+export enum SupportedPlatform {
+    None,
+    Windows,
+    OSX,
+    CentOS,
+    Debian,
+    RHEL,
+    Ubuntu
+}
+
+export function getSupportedPlatform() {
+    if (process.platform === 'win32') {
+        return SupportedPlatform.Windows;
+    }
+    else if (process.platform === 'darwin') {
+        return SupportedPlatform.OSX;
+    }
+    else if (process.platform === 'linux') {
+        // Get the text of /etc/*-release to discover which Linux distribution we're running on.
+        let release = child_process.execSync('cat /etc/*-release').toString().toLowerCase();
+
+        if (release.indexOf('ubuntu') >= 0) {
+            return SupportedPlatform.Ubuntu;
+        }
+        else if (release.indexOf('centos') >= 0) {
+            return SupportedPlatform.CentOS;
+        }
+        else if (release.indexOf('rhel') >= 0) {
+            return SupportedPlatform.RHEL;
+        }
+        else if (release.indexOf('debian') >= 0) {
+            return SupportedPlatform.Debian;
+        }
+    }
+
+    return SupportedPlatform.None;	
+}
