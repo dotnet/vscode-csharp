7de84082636f75021977b95a79f67565d4cb1ec1
Rajkumar Janakiraman <janakiraman.rajkumar@gmail.com>
2016-05-27 13:25:26 -0700

    Schema for pipe transport options. (#376)
    * Schema for pipe transport options.
    
    * Incorporating code review comments.
    
    * Updating package dependency for OpenDebugAD7.
    
    * Updating the version of MIEngine.

--- src/coreclr-debug/main.ts
+++ src/coreclr-debug/main.ts
@@ -333,8 +333,8 @@ function createProjectJson(targetRuntime: string): any
         },
         dependencies: {
             "Microsoft.VisualStudio.clrdbg": "14.0.25320-preview-3008693",
-            "Microsoft.VisualStudio.clrdbg.MIEngine": "14.0.30520-preview-9",
-            "Microsoft.VisualStudio.OpenDebugAD7": "1.0.20520-preview-3",
+            "Microsoft.VisualStudio.clrdbg.MIEngine": "14.0.30527-preview-1",
+            "Microsoft.VisualStudio.OpenDebugAD7": "1.0.20527-preview-1",
             "NETStandard.Library": "1.5.0-rc2-24027",
             "Newtonsoft.Json": "7.0.1",
             "Microsoft.VisualStudio.Debugger.Interop.Portable": "1.0.1",
