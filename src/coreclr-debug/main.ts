49c3e37de96cdca421f152a8c2dfbedec72d2778
Gregg Miskelly <greggm@microsoft.com>
2016-06-06 13:44:03 -0700

    Debugger changes for 1.1.4 (#407)
    This updates the clrdbg package to the 25406 build. This includes fixes
    for Win7 expression evulation and the System.Task breaking change.
    This addresses: https://github.com/OmniSharp/omnisharp-vscode/issues/258
    
    This also the MIEngine to pull in https://github.com/Microsoft/MIEngine/pull/353
    
    Lastly this disables the external console by default. We decided to have it off
    by default as we have seen some issues with it on OSX (ex: not always closing
    when the target exits), and we are hoping it will be possible soon to use the
    new VS Code console window instead. So we decided to turn it off by default, at
    least for now.

--- src/coreclr-debug/main.ts
+++ src/coreclr-debug/main.ts
@@ -336,8 +336,8 @@ function createProjectJson(targetRuntime: string): any
             emitEntryPoint: true
         },
         dependencies: {
-            "Microsoft.VisualStudio.clrdbg": "14.0.25320-preview-3008693",
-            "Microsoft.VisualStudio.clrdbg.MIEngine": "14.0.30531-preview-1",
+            "Microsoft.VisualStudio.clrdbg": "14.0.25406-preview-3044032",
+            "Microsoft.VisualStudio.clrdbg.MIEngine": "14.0.30606-preview-1",
             "Microsoft.VisualStudio.OpenDebugAD7": "1.0.20527-preview-1",
             "NETStandard.Library": "1.5.0-rc2-24027",
             "Newtonsoft.Json": "7.0.1",
