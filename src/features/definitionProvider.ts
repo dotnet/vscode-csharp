083f208ac5bd60c652626ddff6e35d3e1f88db94
Ivan Zlatev <ivan@ivanz.com>
2016-08-16 23:12:56 +0100

    Added support for Peek Definition by implementing the document provider properly

--- src/features/definitionProvider.ts
+++ src/features/definitionProvider.ts
@@ -6,55 +6,47 @@
 'use strict';
 
 import AbstractSupport from './abstractProvider';
-import {MetadataRequest, GoToDefinitionRequest} from '../omnisharp/protocol';
+import {MetadataRequest, GoToDefinitionRequest, MetadataSource} from '../omnisharp/protocol';
 import * as serverUtils from '../omnisharp/utils';
 import {createRequest, toLocation} from '../omnisharp/typeConvertion';
-import {workspace, window, Uri, TextDocument, Position, Location, CancellationToken, DefinitionProvider, Selection, TextEditorRevealType} from 'vscode';
+import {Uri, TextDocument, Position, Location, CancellationToken, DefinitionProvider} from 'vscode';
+import {DefinitionMetadataDocumentProvider} from './definitionMetadataDocumentProvider';
+
 
 export default class CSharpDefinitionProvider extends AbstractSupport implements DefinitionProvider {
+    private _definitionMetadataDocumentProvider: DefinitionMetadataDocumentProvider;
+
+    constructor(server, definitionMetadataDocumentProvider: DefinitionMetadataDocumentProvider) {
+        super(server);
+        this._definitionMetadataDocumentProvider = definitionMetadataDocumentProvider;
+    }
 
 	public provideDefinition(document: TextDocument, position: Position, token: CancellationToken): Promise<Location> {
 
 		let req = <GoToDefinitionRequest>createRequest(document, position);
         req.WantMetadata = true;
 
-		return serverUtils.goToDefinition(this._server, req, token).then(value => {
+		return serverUtils.goToDefinition(this._server, req, token).then(gotoDefinitionResponse => {
 
-			if (value && value.FileName) {
-				return toLocation(value);
-			} else if (value.MetadataSource) {
+			if (gotoDefinitionResponse && gotoDefinitionResponse.FileName) {
+				return toLocation(gotoDefinitionResponse);
+			} else if (gotoDefinitionResponse.MetadataSource) {
+                const metadataSource: MetadataSource = gotoDefinitionResponse.MetadataSource;
 
-                let lineNumber = value.Line;
-                let column = value.Column;
-
-                serverUtils.getMetadata(this._server, <MetadataRequest> {
-                    AssemblyName: value.MetadataSource.AssemblyName,
-                    VersionNumber: value.MetadataSource.VersionNumber,
-                    ProjectName: value.MetadataSource.ProjectName,
-                    Language: value.MetadataSource.Language,
-                    TypeName: value.MetadataSource.TypeName
+                return serverUtils.getMetadata(this._server, <MetadataRequest> {
+                    Timeout: 5000,
+                    AssemblyName: metadataSource.AssemblyName,
+                    VersionNumber: metadataSource.VersionNumber,
+                    ProjectName: metadataSource.ProjectName,
+                    Language: metadataSource.Language,
+                    TypeName: metadataSource.TypeName
                 }).then(metadataResponse => {
                     if (!metadataResponse || !metadataResponse.Source || !metadataResponse.SourceName) {
                         return;
                     }
 
-                    const scheme = "omnisharp-metadata";
-                    let temporaryDocumentContentProviderRegistration = workspace.registerTextDocumentContentProvider(scheme, {
-                        provideTextDocumentContent: function(uri) {
-                            return metadataResponse.Source;
-                        }
-                    });
-
-                    let uri = Uri.parse(scheme + "://" + metadataResponse.SourceName.replace(/\\/g, "/").replace(/(.*)\/(.*)/g, "$1/[metadata] $2"));
-                    workspace.openTextDocument(uri).then(document => {
-                        temporaryDocumentContentProviderRegistration.dispose();
-
-                        window.showTextDocument(document, null, false).then(editor => {
-                            let position = new Position(lineNumber - 1, column - 1);
-                            editor.selection = new Selection(position, position);
-                            editor.revealRange(editor.document.lineAt(position.line).range, TextEditorRevealType.InCenter);
-                        });
-                    });
+                    const uri: Uri = this._definitionMetadataDocumentProvider.addMetadataResponse(metadataResponse);
+                    return new Location(uri, new Position(gotoDefinitionResponse.Line - 1, gotoDefinitionResponse.Column - 1));
                 });
             }
 		});
