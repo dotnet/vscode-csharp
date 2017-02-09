f520c848d7e6648e7dd90984bd9cd7a02f90071e
Christoph Murczek <christoph.murczek@iteratec.at>
2017-02-09 23:39:44 +0100

    create a base for the PR

---
+++ src/features/packageReferenceProvider.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+import AbstractSupport from './abstractProvider';
+
+export default class PackageReferenceProvider extends AbstractSupport {
+
+}
---
+++ test/packageReferenceProvider.test.ts
@@ -0,0 +1,11 @@
+import { should } from 'chai';
+
+import PackageReferenceProvider from '../src/features/packageReferenceProvider';
+
+suite("Resolve package refs and versions from NuGet", () => {
+    suiteSetup(() => should());
+
+    test("", () => {
+
+    });
+});
