5febd3686923c4dde9acc97c19343133d3965811
Christoph Murczek <christoph.murczek@iteratec.at>
2017-02-11 23:16:45 +0100

    test added

--- test/packageReferenceProvider.test.ts
+++ test/packageReferenceProvider.test.ts
@@ -1,11 +1,33 @@
 import { should } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 
+import { Logger } from '../src/logger';
+import { PackageManager } from '../src/packages';
+import { PlatformInformation } from '../src/platform';
 import PackageReferenceProvider from '../src/features/packageReferenceProvider';
+import * as vscode from 'vscode';
 
 suite("Resolve package refs and versions from NuGet", () => {
-    suiteSetup(() => should());
+    let logger: Logger;
+    let platformInfo: PlatformInformation;
 
-    test("", () => {
+    suiteSetup(() => {
+        should();
+        chai.use(chaiAsPromised);
+        let channel: vscode.OutputChannel = vscode.window.createOutputChannel("test");
+        logger = new Logger(text => channel.append(text));
+        PlatformInformation.GetCurrent().then((pi) => platformInfo = pi);
+    });
+
+    test('Retrieve packages from NuGet Gallery', () => {
+        let partialPackageId = 'newtonsoft';
+        let target = new PackageManager(platformInfo, logger);
+        let packagePromise = target.FindPackageIds(logger, partialPackageId);
+        packagePromise.should.be.fulfilled;
+        packagePromise.should.eventually.not.be.empty;
+    });
+
+    test('Retrieve the versions for the selected package', () => {
 
     });
 });
