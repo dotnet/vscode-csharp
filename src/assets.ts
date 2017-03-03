c32f17b7776b523587b2f83b1caee3451911190b
Gregg Miskelly <greggm@microsoft.com>
2017-03-03 11:17:15 -0800

    launch.json fixes for 'cwd' and '${command.' (#1282)
    This resolves:
    https://github.com/OmniSharp/omnisharp-vscode/issues/1275
    https://github.com/OmniSharp/omnisharp-vscode/issues/1083

--- src/assets.ts
+++ src/assets.ts
@@ -179,6 +179,21 @@ export class AssetGenerator {
         return result;
     }
 
+    private computeWorkingDirectory() : string {
+        if (!this.hasProject) {
+            // If there's no target project data, use a placeholder for the path.
+            return '${workspaceRoot}';
+        }
+
+        let result = '${workspaceRoot}';
+
+        if (this.projectPath) {
+            result = path.join(result, path.relative(this.rootPath, this.projectPath));
+        }
+
+        return result;
+    }
+
     private createLaunchConfiguration(): ConsoleLaunchConfiguration {
         return {
             name: '.NET Core Launch (console)',
@@ -187,7 +202,7 @@ export class AssetGenerator {
             preLaunchTask: 'build',
             program: this.computeProgramPath(),
             args: [],
-            cwd: '${workspaceRoot}',
+            cwd: this.computeWorkingDirectory(),
             console: "internalConsole",
             stopAtEntry: false,
             internalConsoleOptions: "openOnSessionStart"
@@ -202,7 +217,7 @@ export class AssetGenerator {
             preLaunchTask: 'build',
             program: this.computeProgramPath(),
             args: [],
-            cwd: '${workspaceRoot}',
+            cwd: this.computeWorkingDirectory(),
             stopAtEntry: false,
             internalConsoleOptions: "openOnSessionStart",
             launchBrowser: {
@@ -233,7 +248,7 @@ export class AssetGenerator {
             name: '.NET Core Attach',
             type: 'coreclr',
             request: 'attach',
-            processId: "${command.pickProcess}"
+            processId: "${command:pickProcess}"
         };
     }
 
