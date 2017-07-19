21c838176dba0e9501a141619359c261876316bc
Gregg Miskelly <greggm@microsoft.com>
2017-07-18 17:37:06 -0700

    Switch to download.visualstudio.microsoft.com part 1 (#1646)
    This checkin in the first part of the work to switch to using download.visualstudio.microsoft.com.
    
    This checkin does two things:
    1. It replaces the OmniSharp links with CDN links
    2. It adds a new gulp task to update links in package.json. This will allow us to script the insertion process.

---
+++ src/tools/UpdatePackageDependencies.ts
@@ -0,0 +1,87 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import { Package } from '../packages';
+
+interface PackageJSONFile
+{
+    runtimeDependencies : Package[];
+}
+
+export function updatePackageDependencies() {
+
+    const urlsIndex = process.argv.indexOf("--urls");
+    const newPrimaryUrls = urlsIndex >= 0 ? process.argv[urlsIndex+1] : undefined;
+    
+    const fallbackUrlsIndex = process.argv.indexOf("--fallbackUrls");
+    const newFallbackUrls = fallbackUrlsIndex >= 0 ? process.argv[fallbackUrlsIndex+1] : undefined;
+    
+    if (newPrimaryUrls === undefined || newPrimaryUrls === "-?" || newPrimaryUrls === "-h") {
+        console.log("This command will update the URLs for package dependencies in package.json");
+        console.log();
+        console.log("Syntax: updatePackageDependencies --urls \"<url1>,<url2>,...\" [--fallbackUrls \"<fallback-url-1>,<fallback-url-2>...\"]");
+        console.log();
+        return;
+    }
+
+    if (newPrimaryUrls.length === 0) {
+        throw new Error("Invalid first argument to updatePackageDependencies. URL string argument expected.");
+    }
+
+    let packageJSON: PackageJSONFile = JSON.parse(fs.readFileSync('package.json').toString());
+    
+    // map from lowercase filename to Package
+    const mapFileNameToDependency = {};
+
+    // First build the map
+    packageJSON.runtimeDependencies.forEach(dependency => {
+        let fileName = getLowercaseFileNameFromUrl(dependency.url);
+        let existingDependency = mapFileNameToDependency[fileName];
+        if (existingDependency !== undefined) {
+            throw new Error(`Multiple dependencies found with filename '${fileName}': '${existingDependency.url}' and '${dependency.url}'.`);
+        }
+        mapFileNameToDependency[fileName] = dependency;
+    });
+    
+    let findDependencyToUpdate = (url : string): Package => {
+        let fileName = getLowercaseFileNameFromUrl(url);
+        let dependency = mapFileNameToDependency[fileName];
+        if (dependency === undefined) {
+            throw new Error(`Unable to update item for url '${url}'. No 'runtimeDependency' found with filename '${fileName}'.`);
+        }
+
+        console.log(`Updating ${url}`);
+        return dependency;
+    };
+
+    newPrimaryUrls.split(',').forEach(urlToUpdate =>{
+        let depedency = findDependencyToUpdate(urlToUpdate);
+        depedency.url = urlToUpdate;
+    });
+
+    if (newFallbackUrls !== undefined) {
+        newFallbackUrls.split(',').forEach(urlToUpdate =>{
+            let depedency = findDependencyToUpdate(urlToUpdate);
+            depedency.fallbackUrl = urlToUpdate;
+        }); 
+    }
+
+    let content = JSON.stringify(packageJSON, null, 2);
+    if (os.platform() === 'win32') {
+        content = content.replace(/\n/gm, "\r\n");
+    }
+    fs.writeFileSync('package.json', content);
+}
+
+function getLowercaseFileNameFromUrl(url : string) : string {
+
+    if (!url.startsWith("https://")) {
+        throw new Error(`Unexpected URL '${url}'. URL expected to start with 'https://'.`);
+    }
+
+    if (!url.endsWith(".zip")) {
+        throw new Error(`Unexpected URL '${url}'. URL expected to end with '.zip'.`);
+    }
+
+    let index = url.lastIndexOf("/");
+    return url.substr(index + 1).toLowerCase();    
+}
