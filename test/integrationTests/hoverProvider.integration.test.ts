13d8a8c2f43337a6ccb732f9a8378aaf3fd4abdb
Akshita Agarwal <akshita.agarwal.cse13@itbhu.ac.in>
2018-02-20 17:13:57 -0800

    Not requiring server restart

--- test/integrationTests/hoverProvider.integration.test.ts
+++ test/integrationTests/hoverProvider.integration.test.ts
@@ -28,7 +28,6 @@ suite(`Hover Provider: ${testAssetWorkspace.description}`, function () {
         }
 
         await csharpExtension.exports.initializationFinished;
-        await omnisharp.restart();
     });
 
     test("Hover returns structured documentation with proper newlines", async function () {
