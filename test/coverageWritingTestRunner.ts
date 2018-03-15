5d1f2026a46ec555ea0b9fa45f32c8bf2539449d
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-03-15 10:03:44 -0700

    Fix codecov integration (#2108)
    Fix js->ts coverage map transformation in Travis CI where `node` is not on the path.

--- test/coverageWritingTestRunner.ts
+++ test/coverageWritingTestRunner.ts
@@ -4,7 +4,9 @@
 
 
 'use strict';
-import {execAsync} from 'async-child-process';
+
+import shelljs = require("async-shelljs");
+
 import path = require('path');
 const fs = require('async-file');
 import Mocha = require('mocha');
@@ -27,17 +29,17 @@ export default class CoverageWritingTestRunner {
 
     public run(testRoot: string, clb: any) {
         let promiseResolve: any;
-        let clbArgsLocal: {error, failures?: number};
+        let clbArgsLocal: { error, failures?: number };
 
-        new Promise<{error, failures?: number}>(function (resolve, reject) {
-            promiseResolve = (error, failures?: number) => resolve({error, failures});
+        new Promise<{ error, failures?: number }>(function (resolve, reject) {
+            promiseResolve = (error, failures?: number) => resolve({ error, failures });
         })
             .then(clbArgs => {
                 clbArgsLocal = clbArgs;
                 return this.writeCoverage();
             })
             .then(() => clb(clbArgsLocal.error, clbArgsLocal.failures),
-                  () => clb(clbArgsLocal.error, clbArgsLocal.failures));
+                () => clb(clbArgsLocal.error, clbArgsLocal.failures));
 
         this.baseRunner.run(testRoot, promiseResolve);
     }
@@ -50,32 +52,58 @@ export default class CoverageWritingTestRunner {
                 await fs.mkdir(nycFolderPath);
             }
 
-            let rawCoverageJsonPath = path.join(nycFolderPath, `${process.env.OSVC_SUITE}.json`);
-            let remappedCoverageJsonPath = path.join(nycFolderPath, `${process.env.OSVC_SUITE}.remapped.json`);
-            let outFolderPath = path.join(process.env.CODE_EXTENSIONS_PATH, "out");
-            let remapIstanbulPath = path.join(process.env.CODE_EXTENSIONS_PATH, "node_modules", "remap-istanbul", "bin", "remap-istanbul.js");
+            let rawCoverageJsonPath: string;
+            let remappedCoverageJsonPath: string;
+            let outFolderPath: string;
+            let remapIstanbulPath: string;
+            let nodePath: string;
 
-            await fs.writeTextFile(rawCoverageJsonPath, JSON.stringify(__coverage__));
-
-
-            let result = await execAsync(`node ${remapIstanbulPath} -i ${rawCoverageJsonPath} -o ${rawCoverageJsonPath}`, {
-                cwd: outFolderPath
-            });
-            let remappedResult = JSON.parse(await fs.readTextFile(rawCoverageJsonPath));
-            let finalResult = {};
-
-            for (let key in remappedResult){
-                if (remappedResult[key].path) {
-                    let realPath = key.replace("../", "./");
-                    finalResult[realPath] = remappedResult[key];
-                    finalResult[realPath].path = realPath;
+            try {
+                rawCoverageJsonPath = path.join(nycFolderPath, `${process.env.OSVC_SUITE}.json`);
+                remappedCoverageJsonPath = path.join(nycFolderPath, `${process.env.OSVC_SUITE}.remapped.json`);
+                outFolderPath = path.join(process.env.CODE_EXTENSIONS_PATH, "out");
+                remapIstanbulPath = path.join(process.env.CODE_EXTENSIONS_PATH, "node_modules", "remap-istanbul", "bin", "remap-istanbul.js");
+                nodePath = process.env.NVM_BIN;
+                if (nodePath) {
+                	nodePath = `${nodePath}${path.delimiter}`;
                 }
-                else {
-                    finalResult[key] = remappedResult[key];
+
+                await fs.writeTextFile(rawCoverageJsonPath, JSON.stringify(__coverage__));
+                
+                let result = await shelljs.asyncExec(`${nodePath}node ${remapIstanbulPath} -i ${rawCoverageJsonPath} -o ${remappedCoverageJsonPath}`, {
+                    cwd: outFolderPath
+                });
+
+                
+
+                let remappedResult = JSON.parse(await fs.readTextFile(remappedCoverageJsonPath));
+
+                let finalResult = {};
+
+                for (let key in remappedResult) {
+                    if (remappedResult[key].path) {
+                        let realPath = key.replace("../", "./");
+
+                        finalResult[realPath] = remappedResult[key];
+                        finalResult[realPath].path = realPath;
+                    }
+                    else {
+                        finalResult[key] = remappedResult[key];
+                    }
                 }
+
+                await fs.writeTextFile(remappedCoverageJsonPath, JSON.stringify(finalResult));
+
+                console.log(`done remapping ${finalResult}`);
             }
-
-            await fs.writeTextFile(rawCoverageJsonPath, JSON.stringify(finalResult));
+            catch (e) {
+                console.log(`Coverage remapping failure: ${JSON.stringify(e)}`);
+                console.log(`* rawCoverageJsonPath: ${rawCoverageJsonPath}`);
+                console.log(`* remappedCoverageJsonPath: ${remappedCoverageJsonPath}`);
+                console.log(`* outFolderPath: ${outFolderPath}`);
+                console.log(`* remapIstanbulPath: ${remapIstanbulPath}`);
+                console.log(`* nodePath: ${nodePath}`);
+            }
         }
     }
 }
