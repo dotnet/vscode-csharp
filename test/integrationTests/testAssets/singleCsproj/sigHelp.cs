da3248956e166577ff017163703c0808d20f5ecd
akshita31 <akagarw@microsoft.com>
2018-03-19 13:05:50 -0700

    Clean up signature help test (#2117)
    * Added parameter
    
    * modifications
    
    * Cleaned up tests

--- test/integrationTests/testAssets/singleCsproj/sigHelp.cs
+++ test/integrationTests/testAssets/singleCsproj/sigHelp.cs
@@ -6,7 +6,7 @@ namespace Test
         ///<summary>DoWork is some method.</summary>
         /// <param name="Int1">Used to indicate status.</param>
         /// <param name="Float1">Used to specify context.</param>
-        public static void DoWork(int Int1, float Float1)
+        public static void DoWork(int Int1, float Float1, double Double1)
         {
         }
 
@@ -16,7 +16,7 @@ namespace Test
 
         public static void main()
         {
-            DoWork(4, 4.0f);
+            DoWork(4, 4.0f, 5.0);
             noDocMethod();
         }
     }
