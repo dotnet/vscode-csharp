424feb9c4283c81ab62ca3b1688ea3f7d17b8fe6
Piotr Puszkiewicz <piotrp@microsoft.com>
2018-04-06 01:21:34 -0700

    noImplicitAny

--- gulpfile.ts
+++ gulpfile.ts
@@ -10,6 +10,8 @@ import * as optionsSchemaGenerator from './src/tools/GenerateOptionsSchema';
 import * as packageDependencyUpdater from './src/tools/UpdatePackageDependencies';
 
 import tslint from 'gulp-tslint';
+import { PlatformInformation } from './src/platform';
+import { PackageManager } from './src/packages';
 
 require('./tasks/testTasks');
 require('./tasks/onlinePackagingTasks');
