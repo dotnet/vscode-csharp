8ea41c797cbf206314f4e209f3e3bc81cf1266f4
Akshita Agarwal <akagarw@microsoft.com>
2018-04-13 15:40:11 -0700

    fix this

--- src/Disposable.ts
+++ src/Disposable.ts
@@ -1,20 +1,20 @@
 /*--------------------------------------------------------------------------------------------- 
  *  Copyright (c) Microsoft Corporation. All rights reserved. 
  *  Licensed under the MIT License. See License.txt in the project root for license information. 
- *--------------------------------------------------------------------------------------------*/ 
- 
-export default class Disposable { 
-    private onDispose: {(): void}; 
- 
-    constructor (onDispose: {(): void}){ 
-        if (!onDispose) { 
-            throw new Error("onDispose cannot be null or empty."); 
-        } 
- 
-        this.onDispose = onDispose; 
-    } 
- 
-    public dispose(): void { 
-        this.onDispose(); 
-    } 
+ *--------------------------------------------------------------------------------------------*/
+
+export default class Disposable {
+    private onDispose: { (): void };
+
+    constructor(onDispose: { (): void }) {
+        if (!onDispose) {
+            throw new Error("onDispose cannot be null or empty.");
+        }
+
+        this.onDispose = onDispose;
+    }
+
+    public dispose = (): void => {
+        this.onDispose();
+    }
 }
