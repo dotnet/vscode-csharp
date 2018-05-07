0ead6289db48049cdf9499ca9c832a437be57123
Akshita Agarwal <akagarw@microsoft.com>
2018-05-07 15:10:20 -0700

    refactoring warning message observer

--- test/unitTests/logging/WarningMessageObserver.test.ts
+++ test/unitTests/logging/WarningMessageObserver.test.ts
@@ -75,82 +75,85 @@ suite('WarningMessageObserver', () => {
     });
 
     [
-        getOmnisharpMSBuildProjectDiagnosticsEvent("someFile",
-            [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
-            [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]),
-        getOmnisharpServerOnErrorEvent("someText", "someFile", 1, 2)
-    ].forEach((event: BaseEvent) => {
-        suite(`${event.constructor.name}`, () => {
+        {
+            eventA: getOmnisharpMSBuildProjectDiagnosticsEvent("someFile",
+                [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
+                [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]),
+
+            eventB: getOmnisharpMSBuildProjectDiagnosticsEvent("BFile",
+                [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
+                [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]),
+
+            eventC: getOmnisharpMSBuildProjectDiagnosticsEvent("CFile",
+                [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
+                [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]),
+            assertion1: '[1] Some projects have trouble loading. Please review the output for more details.',
+            assertion2: '[2] Some projects have trouble loading. Please review the output for more details.',
+            expected: "Some projects have trouble loading. Please review the output for more details.",
+            command: "o.showOutput"
+        },
+        {
+            eventA: getOmnisharpServerOnErrorEvent("someText1", "someFile1", 1, 2),
+            eventB: getOmnisharpServerOnErrorEvent("someText2", "someFile2", 1, 2),
+            eventC: getOmnisharpServerOnErrorEvent("someText3", "someFile3", 1, 2),
+            assertion1: '[1] Some projects have trouble loading. Please review the output for more details.',
+            assertion2: '[2] Some projects have trouble loading. Please review the output for more details.',
+            expected: "Some projects have trouble loading. Please review the output for more details.",
+            command: "o.showOutput"
+        }
+    ].forEach(elem => {
+        suite(`${elem.eventA.constructor.name}`, () => {
             test(`When the event is fired then a warning message is displayed`, () => {
                 let marble = `${timeToMarble(1500)}a`;
-                let marble_event_map = { a: event };
+                let marble_event_map = { a: elem.eventA };
                 let eventList = scheduler.createHotObservable(marble, marble_event_map);
                 eventList.subscribe(e => observer.post(e));
-                scheduler.expectObservable(assertionObservable).toBe(`${timeToMarble(3000)}a`, { a: '[1] Some projects have trouble loading. Please review the output for more details.' });
+                scheduler.expectObservable(assertionObservable).toBe(`${timeToMarble(3000)}a`, { a: elem.assertion1 });
                 scheduler.flush();
-
                 expect(warningMessages.length).to.be.equal(1);
-                expect(warningMessages[0]).to.be.equal("Some projects have trouble loading. Please review the output for more details.");
+                expect(warningMessages[0]).to.be.equal(elem.expected);
             });
 
             test(`When events are fired rapidly, then they are debounced by 1500 ms`, () => {
                 let marble = `${timeToMarble(1000)}a${timeToMarble(500)}b${timeToMarble(500)}c`;
-
-                let eventB = getOmnisharpMSBuildProjectDiagnosticsEvent("BFile",
-                    [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
-                    [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]);
-
-                let eventC = getOmnisharpMSBuildProjectDiagnosticsEvent("CFile",
-                    [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
-                    [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]);
-
-                let marble_event_map = { a: event, b: eventB, c: eventC };
+                let marble_event_map = { a: elem.eventA, b: elem.eventB, c: elem.eventC };
                 let eventList = scheduler.createHotObservable(marble, marble_event_map);
                 eventList.subscribe(e => observer.post(e));
-                scheduler.expectObservable(assertionObservable).toBe(`${timeToMarble(3520)}a`, { a: '[1] Some projects have trouble loading. Please review the output for more details.' });
+                scheduler.expectObservable(assertionObservable).toBe(`${timeToMarble(3520)}a`, { a: elem.assertion1 });
                 scheduler.flush();
 
                 expect(warningMessages.length).to.be.equal(1);
-                expect(warningMessages[0]).to.be.equal("Some projects have trouble loading. Please review the output for more details.");
+                expect(warningMessages[0]).to.be.equal(elem.expected);
             });
 
             test(`When events are 1500 ms apart, then they are not debounced`, () => {
                 let marble = `${timeToMarble(1000)}a${timeToMarble(490)}b${timeToMarble(1500)}c`;
-
-                let eventB = getOmnisharpMSBuildProjectDiagnosticsEvent("BFile",
-                    [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
-                    [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]);
-
-                let eventC = getOmnisharpMSBuildProjectDiagnosticsEvent("CFile",
-                    [getMSBuildDiagnosticsMessage("warningFile", "", "", 1, 2, 3, 4)],
-                    [getMSBuildDiagnosticsMessage("errorFile", "", "", 5, 6, 7, 8)]);
-
-                let marble_event_map = { a: event, b: eventB, c: eventC };
+                let marble_event_map = { a: elem.eventA, b: elem.eventB, c: elem.eventC };
                 let eventList = scheduler.createHotObservable(marble, marble_event_map);
                 eventList.subscribe(e => observer.post(e));
                 scheduler.expectObservable(assertionObservable).toBe(`${timeToMarble(3000)}a${timeToMarble(1500)}b`,
                     {
-                        a: '[1] Some projects have trouble loading. Please review the output for more details.',
-                        b: '[2] Some projects have trouble loading. Please review the output for more details.'
+                        a: elem.assertion1,
+                        b: elem.assertion2
                     });
                 scheduler.flush();
                 expect(warningMessages.length).to.be.equal(2);
-                expect(warningMessages[0]).to.be.equal("Some projects have trouble loading. Please review the output for more details.");
+                expect(warningMessages[0]).to.be.equal(elem.expected);
             });
 
             test(`Given a warning message, when the user clicks ok the command is executed`, async () => {
                 let marble = `${timeToMarble(1500)}a`;
-                let eventList = scheduler.createHotObservable(marble, { a: event });
+                let eventList = scheduler.createHotObservable(marble, { a: elem.eventA });
                 scheduler.expectObservable(eventList.map(e => observer.post(e)));
                 scheduler.flush();
                 doClickOk();
                 await commandDone;
-                expect(invokedCommand).to.be.equal("o.showOutput");
+                expect(invokedCommand).to.be.equal(elem.command);
             });
 
             test(`Given a warning message, when the user clicks cancel the command is not executed`, async () => {
                 let marble = `${timeToMarble(1500)}a--|`;
-                let eventList = scheduler.createHotObservable(marble, { a: event });
+                let eventList = scheduler.createHotObservable(marble, { a: elem.eventA });
                 scheduler.expectObservable(eventList.map(e => observer.post(e)));
                 scheduler.flush();
                 doClickCancel();
@@ -181,6 +184,7 @@ suite('WarningMessageObserver', () => {
                 expect(warningMessages[0]).to.be.equal("OmniSharp configuration has changed. Would you like to relaunch the OmniSharp server with your changes?");
             });
         });
+
         [
             { section: 'omnisharp', config: 'loggingLevel', value: "debug" },
             { section: 'omnisharp', config: 'autoStart', value: false },
@@ -203,7 +207,6 @@ suite('WarningMessageObserver', () => {
                 expect(warningMessages).to.be.empty;
             });
         });
-
     });
 
     function getVSCodeForWarningMessage(): vscode {
