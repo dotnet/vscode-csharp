75cd4e2d2707021804616abd1a6d56b335fcecdd
Akshita Agarwal <akagarw@microsoft.com>
2018-05-08 14:59:44 -0700

    Remove unnecessary using

--- test/unitTests/logging/WarningMessageObserver.test.ts
+++ test/unitTests/logging/WarningMessageObserver.test.ts
@@ -6,7 +6,6 @@
 import { WarningMessageObserver } from '../../../src/observers/WarningMessageObserver';
 import { assert, use as chaiUse, expect, should } from 'chai';
 import { getFakeVsCode, getMSBuildDiagnosticsMessage, getOmnisharpMSBuildProjectDiagnosticsEvent, getOmnisharpServerOnErrorEvent } from '../testAssets/Fakes';
-import { BaseEvent } from '../../../src/omnisharp/loggingEvents';
 import { vscode } from '../../../src/vscodeAdapter';
 import { TestScheduler } from 'rxjs/testing/TestScheduler';
 import { Observable } from 'rxjs/Observable';
