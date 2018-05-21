94927041f29476334897ddc03258ab3d872dbc37
Akshita Agarwal <akagarw@microsoft.com>
2018-05-21 15:44:39 -0700

    channel test

---
+++ test/unitTests/logging/DotnetTestChannelObserver.test.ts
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { expect } from 'chai';
+import { getNullChannel } from '../testAssets/Fakes';
+import { BaseEvent, DotnetTestsInClassDebugStart, DotnetTestRunStart, DotnetTestRunFailure, DotnetTestsInClassRunStart, DotnetTestDebugStart } from '../../../src/omnisharp/loggingEvents';
+import DotnetTestChannelObserver from '../../../src/observers/DotnetTestChannelObserver';
+
+suite("DotnetTestChannelObserver", () => {
+    let hasShown: boolean;
+
+    let observer = new DotnetTestChannelObserver({
+        ...getNullChannel(),
+        show: () => { hasShown = true; }
+    });
+
+    setup(() => {
+        hasShown = false;
+    });
+
+    [
+        new DotnetTestRunStart("foo"),
+        new DotnetTestRunFailure("some failure"),   
+        new DotnetTestsInClassRunStart(), 
+        new DotnetTestDebugStart("foo"),
+        new DotnetTestsInClassDebugStart()
+    ].forEach((event: BaseEvent) => {
+        test(`CommandDotNetRestoreStart : Clears and shows the channel`, () => {
+            expect(hasShown).to.be.false;
+            observer.post(event);
+            expect(hasShown).to.be.true;
+        });
+    });
+});
