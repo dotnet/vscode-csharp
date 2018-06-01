5211d5bf98f111d9ba180295abb1a50933e209b0
Diego <dicolomb@microsoft.com>
2018-06-01 12:16:48 +0100

    setup for sonar qube integration
    add parameters to script
    
    
    import coverage
    
    
    add branch paramter
    
    
    removed token

---
+++ sonar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function readJson {
+  UNAMESTR=`uname`
+  if [[ "$UNAMESTR" == 'Linux' ]]; then
+    SED_EXTENDED='-r'
+  elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+    SED_EXTENDED='-E'
+  fi;
+
+  VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
+
+  if [ ! "$VALUE" ]; then
+    echo "Error: Cannot find \"${2}\" in ${1}" >&2;
+    exit 1;
+  else
+    echo $VALUE ;
+  fi;
+}
+
+VERSION=`readJson package.json version` || exit 1;
+
+sonar-scanner -X -Dsonar.projectVersion=$VERSION -Dproject.settings=./sonar-project.properties
