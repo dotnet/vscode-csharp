8ca122852f7bbe4b235b1dde1a0288e14944a6cf
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-10-01 11:39:46 +0100

    Update to newer Razor activation signature

--- src/razor/razor.ts
+++ src/razor/razor.ts
@@ -7,14 +7,15 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import * as Razor from 'microsoft.aspnetcore.razor.vscode';
+import { EventStream } from '../EventStream';
 
-export async function activateRazorExtension(context: vscode.ExtensionContext) {
+export async function activateRazorExtension(context: vscode.ExtensionContext, eventStream: EventStream) {
     const razorConfig = vscode.workspace.getConfiguration('razor');
     const configuredLanguageServerDir = razorConfig.get<string>('languageServer.directory', null);
     const languageServerDir = configuredLanguageServerDir || path.join(context.extensionPath, '.razor');
 
     if (fs.existsSync(languageServerDir)) {
-        await Razor.activate(context, languageServerDir);
+        await Razor.activate(context, languageServerDir, eventStream);
     } else if (configuredLanguageServerDir) {
         // It's only an error if the nonexistent dir was explicitly configured
         // If it's the default dir, this is expected for unsupported platforms
