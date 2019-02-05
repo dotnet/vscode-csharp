f2e72fd5b05b3fb6b1237561d02710ca1c8be7a5
Akshita Agarwal <akagarw@microsoft.com>
2019-02-04 18:16:57 -0800

    clean up

--- tes-script.ps1
+++
@@ -1,9 +0,0 @@
-#npm i
-rm .\csharp-1.18.0-beta6.vsix
-gulp vsix:release:package
-gulp vsix:release:unpackage
-code --uninstall-extension csharp-1.18.0-beta6.vsix
-rm C:\Users\akagarw\.vscode\extensions\ms-vscode.csharp-1.18.0-beta6
-code --install-extension ./csharp-1.18.0-beta6.vsix
-code C:\Users\akagarw\Desktop\CodeSamples\test
-#gulp test:integration:singleCsproj --codeExtensionPath ./vsix/extension
