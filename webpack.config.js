9cc660cc3483668017eeb4084cd26b207d30a95a
Akshita <akagarw@microsoft.com>
2019-03-18 16:44:22 -0700

    remove the changes from the testRunner

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,6 @@ const config = {
             exclude: /node_modules/,
             use: [{
                 loader: 'ts-loader',
-                options: {
-                    compilerOptions: {
-                        "module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
-                    }
-                }
             }]
         }]
     },
