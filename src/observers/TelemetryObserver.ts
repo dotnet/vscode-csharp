1ab067fbcdf3038246fe5f86624f775915ee0308
Akshita <akagarw@microsoft.com>
2019-04-04 16:28:54 -0700

    Pipe demilimted

--- src/observers/TelemetryObserver.ts
+++ src/observers/TelemetryObserver.ts
@@ -47,9 +47,9 @@ export class TelemetryObserver {
             case EventType.ProjectConfigurationReceived:
                 let projectConfig = (<ProjectConfiguration>event).projectConfiguration;
                 telemetryProps['ProjectGuid'] = projectConfig.ProjectGuid;
-                telemetryProps['TargetFrameworks'] = projectConfig.TargetFrameworks.join(";");
-                telemetryProps['References'] = projectConfig.References.join(";");
-                telemetryProps['FileExtensions'] = projectConfig.FileExtensions.join(";");
+                telemetryProps['TargetFrameworks'] = projectConfig.TargetFrameworks.join("|");
+                telemetryProps['References'] = projectConfig.References.join("|");
+                telemetryProps['FileExtensions'] = projectConfig.FileExtensions.join("|");
                 this.reporter.sendTelemetryEvent("ProjectConfiguration", telemetryProps);
                 break;
         }
