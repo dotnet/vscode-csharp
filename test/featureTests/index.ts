f40410a3fa68d7d6d6d0f08ca48bd0ac959659f5
Joey Robichaud <jorobich@microsoft.com>
2021-04-20 10:34:55 -0700

    Move off of the deprecated vscode package

--- test/featureTests/index.ts
+++ test/featureTests/index.ts
@@ -3,27 +3,60 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-//
-// PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
-//
-// This file is providing the test runner to use when running extension tests.
-// By default the test runner in use is Mocha based.
-//
-// You can provide your own test runner if you want to override it by exporting
-// a function run(testRoot: string, clb: (error:Error) => void) that the extension
-// host can call to run the tests. The test runner is expected to use console.log
-// to report the results back to the caller. When the tests are finished, return
-// a possible error to the callback or null if none.
+import * as fs from "fs";
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
 
-let testRunner = require('vscode/lib/testrunner');
+import { SubscribeToAllLoggers } from "../../src/logger";
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+function setupLogging() {
+    if (process.env.CODE_EXTENSIONS_PATH && process.env.OSVC_SUITE) {
+        let logDirPath = path.join(process.env.CODE_EXTENSIONS_PATH, "./.logs");
 
-testRunner.configure({
-    timeout: 100000,
-    ui: 'tdd',      // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-});
+        if (!fs.existsSync(logDirPath)) {
+            fs.mkdirSync(logDirPath);
+        }
 
-module.exports = testRunner;
+        let logFilePath = path.join(logDirPath, `${process.env.OSVC_SUITE}.log`);
+
+        SubscribeToAllLoggers(message => fs.appendFileSync(logFilePath, message));
+    }
+}
+
+export async function run(): Promise<void> {
+    setupLogging();
+
+    // Create the mocha test
+    const mocha = new Mocha({
+        timeout: 100000,
+        ui: 'tdd',
+        useColors: true
+    });
+
+    const testsRoot = path.resolve(__dirname, '.');
+
+    return new Promise((c, e) => {
+        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+            if (err) {
+                return e(err);
+            }
+
+            // Add files to the test suite
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+            try {
+                // Run the mocha test
+                mocha.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
+            }
+        });
+    });
+}
