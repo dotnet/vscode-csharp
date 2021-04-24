3fbee7eb80e82f0cd982281cc455965ceed7f5c9
Harald Heigl <hh-developing@heigl-online.at>
2021-04-24 05:08:13 +0200

    added debugging for armv7l and aarch64

--- src/main.ts
+++ src/main.ts
@@ -221,7 +221,9 @@ function isSupportedPlatform(platform: PlatformInformation): boolean {
     if (platform.isLinux()) {
         return platform.architecture === "x86_64" ||
             platform.architecture === "x86" ||
-            platform.architecture === "i686";
+            platform.architecture === "i686" ||
+            platform.architecture === "aarch64" ||
+            platform.architecture === "armv7l";
     }
 
     return false;
