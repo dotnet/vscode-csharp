7f0816447679f41cb81cd2d87de259b0880ee6ac
Pascal Senn <senn.pasc@gmail.com>
2022-02-11 00:58:26 +0100

    Performance improvement

--- src/features/testingProvider.ts
+++ src/features/testingProvider.ts
@@ -20,6 +20,7 @@ import TestManager from "./dotnetTest";
 import { Subject } from "rxjs";
 import {
     buffer,
+    concatMap,
     debounceTime,
     distinct,
     filter,
@@ -46,6 +47,7 @@ export default class TestingProvider extends AbstractProvider {
     public controller: vscode.TestController;
 
     private readonly _fileChangeDebouncer = new Subject<string>();
+    private readonly _fileSaveDebouncer = new Subject<string>();
 
     private readonly _projectChangedDebouncer = new Subject<MSBuildProject>();
 
@@ -68,18 +70,28 @@ export default class TestingProvider extends AbstractProvider {
                 distinct(null, flushFiles),
                 buffer(flushFiles),
                 filter((x) => x.length > 0),
-                mergeMap((files) => void this._reportFileChanges(files))
+                mergeMap(async (files) => this._reportFileChanges(files))
+            )
+            .subscribe();
+        const flushSaved = this._fileSaveDebouncer.pipe(debounceTime(1500));
+        const s2 = this._fileSaveDebouncer
+            .pipe(
+                filter((x) => x.endsWith(".cs")),
+                distinct(null, flushSaved),
+                buffer(flushSaved),
+                filter((x) => x.length > 0),
+                mergeMap(async (files) => this._reportFileSaves(files))
             )
             .subscribe();
         const flushProjects = this._projectChangedDebouncer.pipe(
             debounceTime(1500)
         );
-        const s2 = this._projectChangedDebouncer
+        const s3 = this._projectChangedDebouncer
             .pipe(
                 // TODO this is obviously a bad idea
                 filter((x) => x.AssemblyName.endsWith("Tests")),
                 distinct((x) => x.Path, flushProjects),
-                mergeMap((projects) => void this._reportProject(projects))
+                concatMap(async (project) => this._reportProject(project))
             )
             .subscribe();
         const d1 = this.controller.createRunProfile(
@@ -101,11 +113,14 @@ export default class TestingProvider extends AbstractProvider {
         const d5 = server.onProjectRemoved(
             (p) => void this.reportProject(p.MsBuildProject)
         );
-        const d6 = vscode.workspace.onDidOpenTextDocument(
-            (e) => void this.reportFileChange(e.fileName)
-        );
-        const d7 = vscode.workspace.onDidChangeTextDocument(
+        const d6 = vscode.workspace.onDidChangeTextDocument(
             (e) => void this.reportFileChange(e.document.fileName)
+        );
+        const d7 = vscode.workspace.onDidSaveTextDocument(
+            (e) => void this.reportFileSave(e.fileName)
+        );
+        const d8 = vscode.workspace.onDidDeleteFiles(
+            (e) => void this.reportFileDeletes(e.files.map((x) => x.path))
         );
         this.addDisposables(
             new CompositeDisposable(
@@ -117,8 +132,10 @@ export default class TestingProvider extends AbstractProvider {
                 d5,
                 d6,
                 d7,
+                d8,
                 new Disposable(s1),
-                new Disposable(s2)
+                new Disposable(s2),
+                new Disposable(s3)
             )
         );
     }
@@ -139,7 +156,34 @@ export default class TestingProvider extends AbstractProvider {
         this._fileChangeDebouncer.next(fileName);
     }
 
+    public reportFileSave(fileName: string) {
+        this._fileSaveDebouncer.next(fileName);
+    }
+
+    public reportFileDeletes(fileNames: string[]) {
+        const uniqueFileNames = new Set(
+            fileNames.filter((x) => x.endsWith(".cs"))
+        );
+        if (uniqueFileNames.size > 0) {
+            for (const assembly of this._testAssemblies.values()) {
+                let hasChanged = false;
+                for (const test of assembly.discoveredTests.values()) {
+                    if (uniqueFileNames.has(test.CodeFilePath)) {
+                        hasChanged = true;
+                        assembly.discoveredTests.delete(test.id);
+                    }
+                }
+                if (hasChanged) {
+                    this._upsertAssemblyOnController(assembly);
+                }
+            }
+        }
+    }
+
     private async _reportFileChanges(fileNames: string[]): Promise<void> {
+        const inspector = await TestFileInspector.load(this._server, fileNames);
+        const testFiles = inspector.getAllTests();
+
         for (const fileName of new Set(fileNames)) {
             let projectInfo: ProjectInformationResponse;
             try {
@@ -150,8 +194,68 @@ export default class TestingProvider extends AbstractProvider {
             } catch (error) {
                 return undefined;
             }
-            this.reportProject(projectInfo.MsBuildProject);
+
+            this._tryUpdateTestFile(projectInfo, fileName, testFiles);
         }
+    }
+
+    private async _reportFileSaves(fileNames: string[]): Promise<void> {
+        const inspector = await TestFileInspector.load(this._server, fileNames);
+        const testFiles = inspector.getAllTests();
+
+        for (const fileName of new Set(fileNames)) {
+            let projectInfo: ProjectInformationResponse;
+            try {
+                projectInfo = await serverUtils.requestProjectInformation(
+                    this._server,
+                    { FileName: fileName }
+                );
+            } catch (error) {
+                return undefined;
+            }
+
+            if (!this._tryUpdateTestFile(projectInfo, fileName, testFiles)) {
+                this.reportProject(projectInfo.MsBuildProject);
+            }
+        }
+    }
+
+    private _tryUpdateTestFile(
+        projectInfo: ProjectInformationResponse,
+        fileName: string,
+        testFiles: TestInfo[]
+    ): boolean {
+        const assembly = this._testAssemblies.get(
+            projectInfo.MsBuildProject.AssemblyName
+        );
+        if (!assembly) {
+            return false;
+        }
+        const testsFromThisFile = [...assembly.discoveredTests.values()]
+            .filter((x) => x.CodeFilePath == fileName)
+            .sort((x, y) =>
+                x.FullyQualifiedName.localeCompare(y.FullyQualifiedName)
+            );
+        const testFromInspector = testFiles
+            .filter((x) => x.fileName == fileName)
+            .sort((x, y) => x.testMethodName.localeCompare(y.testMethodName));
+
+        if (testsFromThisFile.length != testFromInspector.length) {
+            return false;
+        }
+
+        for (let i = 0; i < testFromInspector.length; i++) {
+            if (
+                testsFromThisFile[i].FullyQualifiedName !=
+                testFromInspector[i].testMethodName
+            ) {
+                return false;
+            }
+            testsFromThisFile[i].discoveryInfo = testFromInspector[i];
+        }
+
+        this._upsertAssemblyOnController(assembly);
+        return true;
     }
 
     public async removeProject(project: MSBuildProject): Promise<void> {
@@ -265,7 +369,7 @@ export default class TestingProvider extends AbstractProvider {
             this._optionProvider.GetLatestOptions().testMaxDegreeOfParallelism,
             token
         );
-    }
+    };
 
     private _processDebugRequest = async (
         request: vscode.TestRunRequest,
@@ -278,7 +382,7 @@ export default class TestingProvider extends AbstractProvider {
             this._server
         );
         await runner.debugTests(token);
-    }
+    };
 
     private _upsertAssemblyOnController(assembly: TestAssembly): void {
         const assemblyTestCollection = this.controller.createTestItem(
@@ -497,20 +601,29 @@ class TestRunner {
 
     public async debugTests(token: vscode.CancellationToken): Promise<void> {
         const testItemsToExecute = this._extractTestItemsFromRequest();
-        testItemsToExecute.forEach(this._testRun.enqueued);
         const executableTests = this._createExecutableTests(testItemsToExecute);
         const testQueue = TestQueue.create(executableTests);
 
-        while (!testQueue.isEmpty() && !token.isCancellationRequested) {
-            const batch = testQueue.dequeueBatch();
-            await this._testAapter.testManager.debugDotnetTestsInClass(
-                "",
-                batch.tests.map(
-                    (assembly) => assembly.testCase.FullyQualifiedName
-                ),
-                batch.assemblyName,
-                batch.testFramework
-            );
+        try {
+            while (!testQueue.isEmpty() && !token.isCancellationRequested) {
+                const batch = testQueue.dequeueBatch();
+                const firstFileName = batch.tests
+                    .map((x) => x.testCase.CodeFilePath)
+                    .filter((x) => !!x)?.[0];
+                if (!firstFileName) {
+                    return;
+                }
+                await this._testAapter.testManager.debugDotnetTestsInClass(
+                    "",
+                    batch.tests.map(
+                        (assembly) => assembly.testCase.FullyQualifiedName
+                    ),
+                    firstFileName,
+                    batch.testFramework
+                );
+            }
+        } finally {
+            this._testRun.end();
         }
     }
 
@@ -790,6 +903,10 @@ class TestFileInspector {
         private readonly _loadedTests: Map<string, TestInfo>
     ) {}
 
+    public getAllTests() {
+        return [...this._loadedTests.values()];
+    }
+
     public getTestInfo(methodName: string) {
         return this._loadedTests.get(methodName);
     }
@@ -815,7 +932,10 @@ class TestFileInspector {
                     token
                 );
                 Structure.walkCodeElements(structure.Elements, (e) => {
-                    for (const info of TestFileInspector._createTestInfo(e)) {
+                    for (const info of TestFileInspector._createTestInfo(
+                        file,
+                        e
+                    )) {
                         if ("name" in info) {
                             loadedFiles.set(info.name, info);
                         } else {
@@ -879,6 +999,7 @@ class TestFileInspector {
     }
 
     private static _createTestInfo(
+        fileName: string,
         element: Structure.CodeElement
     ): TestDiscoveryInfo[] {
         const results: TestDiscoveryInfo[] = [];
@@ -893,6 +1014,7 @@ class TestFileInspector {
                     range,
                     testFramework,
                     testMethodName,
+                    fileName,
                     kind: "method",
                 };
                 results.push(info);
@@ -923,6 +1045,7 @@ class TestFileInspector {
             const info: TestFile = {
                 kind: "file",
                 name: element.Name,
+                fileName,
                 range,
                 testFramework,
                 testMethodNames,
@@ -940,6 +1063,7 @@ interface TestInfo {
     range: V2.Range;
     testFramework: string;
     testMethodName: string;
+    fileName: string;
     kind: "method";
 }
 
@@ -947,6 +1071,7 @@ interface TestFile {
     range: V2.Range;
     testFramework: string;
     testMethodNames: string[];
+    fileName: string;
     name: string;
     kind: "file";
 }
