92045683147b18632f3b2171d015defc1228b949
Winston Liu <Wliu1402@gmail.com>
2022-05-16 20:10:55 -0700

    Update *Adapter types

--- src/vscodeAdapter.ts
+++ src/vscodeAdapter.ts
@@ -234,7 +234,7 @@ export interface StatusBarItem {
      * The priority of this item. Higher value means the item should
      * be shown more to the left.
      */
-    readonly priority: number;
+    readonly priority: number | undefined;
 
     /**
      * The text to show for the entry. You can embed icons in the text by leveraging the syntax:
