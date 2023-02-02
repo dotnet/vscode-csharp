8b51194c693deba61700adb0b6e6a21e4300e67b
Allison Chou <allichou@microsoft.com>
2023-02-02 14:21:49 -0800

    Fix test

--- test/artifactTests/vsix.test.ts
+++ test/artifactTests/vsix.test.ts
@@ -22,7 +22,8 @@ suite("Omnisharp-Vscode VSIX", async () => {
 
     vsixFiles.forEach(element => {
         // We're packaging the platform specific Roslyn server with ready to run in the vsix, so the size should be roughly ~50MB
-        const sizeInMB = 50;
+        // We also publish the Razor server, which is roughly ~75MB
+        const sizeInMB = 125;
         const maximumVsixSizeInBytes = sizeInMB * 1024 * 1024;
 
         suite(`Given ${element}`, () => {
