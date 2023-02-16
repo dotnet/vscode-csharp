d18bc7f795e0fa6de9614c4744605ab0f58d90de
David Barbet <dabarbet@microsoft.com>
2023-02-16 21:00:28 +0000

    Merged PR 451301: Connect to Green via brokered services
    Related - https://devdiv.visualstudio.com/DevDiv/_git/vs-green-server/pullrequest/451302

--- src/lsptoolshost/roslynLanguageServer.ts
+++ src/lsptoolshost/roslynLanguageServer.ts
@@ -18,23 +18,55 @@ import {
 import { PlatformInformation } from '../shared/platform';
 import { DotnetResolver } from '../shared/DotnetResolver';
 import OptionProvider from '../shared/observers/OptionProvider';
+import ShowInformationMessage from '../shared/observers/utils/ShowInformationMessage';
 
 let _languageServer: RoslynLanguageServer;
 let _channel: vscode.OutputChannel;
 let _traceChannel: vscode.OutputChannel;
 
-export class RoslynLanguageServer {
+const greenExtensionId = "ms-dotnettools.visual-studio-green";
 
+export class RoslynLanguageServer {
     /**
      * The timeout for stopping the language server (in ms).
      */
     private static _stopTimeout: number = 10000;
     private _languageClient: LanguageClient | undefined;
 
+    /**
+     * Flag indicating if green was installed the last time we activated.
+     * Used to determine if we need to restart the server on extension changes.
+     */
+    private _wasActivatedWithGreen: boolean | undefined;
+
     constructor(
         private platformInfo: PlatformInformation,
         private optionProvider: OptionProvider,
-    ) { }
+        private context: vscode.ExtensionContext,
+    ) {
+        // subscribe to extension change events so that we can get notified if green is added/removed later.
+        this.context.subscriptions.push(vscode.extensions.onDidChange(async () => {
+            let vsGreenExtension = vscode.extensions.getExtension(greenExtensionId);
+            
+            if (this._wasActivatedWithGreen === undefined) {
+                // Haven't activated yet.
+                return;
+            }
+
+            const title = 'Restart Language Server';
+            const command = 'dotnet.restartServer';
+            if (vsGreenExtension && !this._wasActivatedWithGreen) {
+                // We previously started without green and its now installed.
+                // Offer a prompt to restart the server to use green.
+                _channel.appendLine(`Detected new installation of ${greenExtensionId}`);
+                let message = `Detected installation of ${greenExtensionId}. Would you like to relaunch the language server for added features?`;
+                ShowInformationMessage(vscode, message, { title , command  });
+            } else {
+                // Any other change to extensions is irrelevant - an uninstall requires a reload of the window
+                // which will automatically restart this extension too.
+            }
+        }));
+    }
 
     /**
      * Resolves server options and starts the dotnet language server process.
@@ -46,14 +78,14 @@ export class RoslynLanguageServer {
         let resolvedDotnet = await dotnetResolver.getHostExecutableInfo(options);
         _channel.appendLine("Dotnet version: " + resolvedDotnet.version);
 
-        let solutionPath = await vscode.workspace.findFiles('*.sln', '**/node_modules/**', 1);
-        _channel.appendLine(`Found solution ${solutionPath[0]}`);
+        let solutions = await vscode.workspace.findFiles('*.sln', '**/node_modules/**', 1);
+        let solutionPath = solutions[0];
+        _channel.appendLine(`Found solution ${solutionPath}`);
 
         let logLevel = options.languageServerOptions.logLevel;
 
         let serverOptions: ServerOptions = async () => {
-            const process = this.startServer(solutionPath[0], logLevel);
-            return Promise.resolve<cp.ChildProcess>(process);
+            return await this.startServer(solutionPath, logLevel);
         };
 
         // Options to control the language client
@@ -109,9 +141,9 @@ export class RoslynLanguageServer {
         await this.start();
     }
 
-    private startServer(solutionPath: vscode.Uri, logLevel: string | undefined) : cp.ChildProcess {
+    private async startServer(solutionPath: vscode.Uri, logLevel: string | undefined) : Promise<cp.ChildProcess> {
         let clientRoot = __dirname;
-        
+
         // This environment variable is used by F5 builds to launch the server from the local build directory.
         let serverDirectory = process.env.ROSLYN_LANGUAGE_SERVER_DIRECTORY
             ? path.join(clientRoot, "..", process.env.ROSLYN_LANGUAGE_SERVER_DIRECTORY)
@@ -121,6 +153,11 @@ export class RoslynLanguageServer {
         if (!fs.existsSync(serverPath)) {
             throw new Error(`Cannot find language server in path '${serverPath}''`);
         }
+
+        // Get the brokered service pipe name from green (if installed).
+        // We explicitly call this in the LSP server start action instead of awaiting it
+        // in our activation because Green depends on Blue activation completing.
+        let brokeredServicePipeName = await this.waitForGreenActivationAndGetPipeName();
     
         let args: string[] = [
             serverPath,
@@ -137,9 +174,33 @@ export class RoslynLanguageServer {
         {
             args.push("--logLevel", logLevel);
         }
+
+        if (brokeredServicePipeName) {
+            args.push("--brokeredServicePipeName", brokeredServicePipeName);
+        }
     
         let childProcess = cp.spawn('dotnet', args);
         return childProcess;
+    }
+
+    private async waitForGreenActivationAndGetPipeName(): Promise<string | undefined> {
+        let vsGreenExtension = vscode.extensions.getExtension(greenExtensionId);
+        if (!vsGreenExtension) {
+            // VS green is not installed - continue blue-only activation.
+            _channel.appendLine("Activating Blue standalone...");
+            this._wasActivatedWithGreen = false;
+            return undefined;
+        }
+    
+        _channel.appendLine("Activating Blue + Green...");
+        this._wasActivatedWithGreen = true;
+        let vsGreenExports = await vsGreenExtension.activate();
+        if (!('getBrokeredServiceServerPipeName' in vsGreenExports)) {
+            throw new Error("VS Green is installed but missing expected export getBrokeredServiceServerPipeName");
+        }
+    
+        let brokeredServicePipeName = await vsGreenExports.getBrokeredServiceServerPipeName();
+        return brokeredServicePipeName;
     }
 }
 
@@ -150,14 +211,13 @@ export async function activateRoslynLanguageServer(context: vscode.ExtensionCont
     // Create a separate channel for outputting trace logs - these are incredibly verbose and make other logs very difficult to see.
     _traceChannel = vscode.window.createOutputChannel("C# LSP Trace Logs");
 
-    _languageServer = new RoslynLanguageServer(platformInfo, optionsProvider);
+    _languageServer = new RoslynLanguageServer(platformInfo, optionsProvider, context);
 
     // Register any commands that need to be handled by the extension.
     registerCommands(context, _languageServer);
 
     // Start the language server.
     await _languageServer.start();
-    
 }
 
 // this method is called when your extension is deactivated
