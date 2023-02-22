656ddf8f40cc6d5fdeeafd798d337ceb3091e780
Carlos Adrian Guerra Vazquez <cguerravazqu@microsoft.com>
2023-02-21 17:57:15 -0800

    moved check into a function to be able to re-ask if the user clicks on more info option

--- src/coreclr-debug/debugConfigurationProvider.ts
+++ src/coreclr-debug/debugConfigurationProvider.ts
@@ -75,35 +75,44 @@ export class DotnetDebugConfigurationProvider implements vscode.DebugConfigurati
 
             if (debugConfiguration.checkForDevCert)
             {
-                hasDotnetDevCertsHttps(this.options.dotNetCliPaths).then(async (returnData) => {
-                    if(returnData.error) //if the prcess returns 0 error is null, otherwise the return code can ba acessed in returnData.error.code
-                    {
-                        const result = await vscode.window.showInformationMessage(
-                            "The selected launch configuration is configured to launch a web browser but no trusted development certificate was found. Create a trusted self-signed certificate?", 
-                            { title:"Yes"}, { title:'Not Now', isCloseAffordance: true }, { title:"More Information"}
-                            ); 
-                        if (result?.title === 'Yes')
-                        {
-                            let returnData = await createSelfSignedCert(this.options.dotNetCliPaths);
-                            if (returnData.error === null)
-                            {
-                                vscode.window.showInformationMessage('Self-signed certificate sucessfully created');
-                            }
-                            else
-                            {
-                                vscode.window.showWarningMessage("Couldn't create self-signed certificate");
-                            }
-                        }
-                        if (result?.title === "More Information")
-                        {
-                            const launchjsonDescriptionURL = 'https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md#check-for-devcert';
-                            vscode.env.openExternal(vscode.Uri.parse(launchjsonDescriptionURL));
-                        }
-                    }
-                });
+                checkForDevCerts(this.options.dotNetCliPaths);
             }
         }
 
         return debugConfiguration;
     }
+}
+
+function checkForDevCerts(dotNetCliPaths: string[]){
+    hasDotnetDevCertsHttps(dotNetCliPaths).then(async (returnData) => {
+        if(returnData.error) //if the prcess returns 0 error is null, otherwise the return code can ba acessed in returnData.error.code
+        {
+            const labelYes: string = "Yes";
+            const labelNotNow: string = "Not Now";
+            const labelMoreInfo: string = "More Information";
+
+            const result = await vscode.window.showInformationMessage(
+                "The selected launch configuration is configured to launch a web browser but no trusted development certificate was found. Create a trusted self-signed certificate?", 
+                { title:labelYes}, { title:labelNotNow, isCloseAffordance: true }, { title:labelMoreInfo}
+                ); 
+            if (result?.title === labelYes)
+            {
+                let returnData = await createSelfSignedCert(dotNetCliPaths);
+                if (returnData.error === null)
+                {
+                    vscode.window.showInformationMessage('Self-signed certificate sucessfully created');
+                }
+                else
+                {
+                    vscode.window.showWarningMessage("Couldn't create self-signed certificate");
+                }
+            }
+            if (result?.title === labelMoreInfo)
+            {
+                const launchjsonDescriptionURL = 'https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md#check-for-devcert';
+                vscode.env.openExternal(vscode.Uri.parse(launchjsonDescriptionURL));
+                checkForDevCerts(dotNetCliPaths);
+            }
+        }
+    });
 }
