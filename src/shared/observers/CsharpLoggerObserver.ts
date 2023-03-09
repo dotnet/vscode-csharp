35eb30ed2f6832d3e03be55a815d060db31d906d
David Barbet <dabarbet@microsoft.com>
2023-03-09 23:11:11 +0000

    Merged PR 457201: Merge latest O# in

--- src/shared/observers/CsharpLoggerObserver.ts
+++ src/shared/observers/CsharpLoggerObserver.ts
@@ -68,6 +68,9 @@ export class CsharpLoggerObserver extends BaseLoggerObserver {
             case EventType.IntegrityCheckSuccess:
                 this.handleIntegrityCheckSuccess(<Event.IntegrityCheckSuccess>event);
                 break;
+            case EventType.DevCertCreationFailure:
+                this.handleDevCertCreationFailure(<Event.DevCertCreationFailure>event);
+                break;
         }
     }
 
@@ -145,5 +148,9 @@ export class CsharpLoggerObserver extends BaseLoggerObserver {
 
     private handleDocumentSynchronizationFailure(event: Event.DocumentSynchronizationFailure) {
         this.logger.appendLine(`Failed to synchronize document '${event.documentPath}': ${event.errorMessage}`);
+    }
+
+    private handleDevCertCreationFailure(event: Event.DevCertCreationFailure) {
+        this.logger.appendLine(`Couldn't create self-signed certificate. ${event.errorMessage}`);
     }
 }
