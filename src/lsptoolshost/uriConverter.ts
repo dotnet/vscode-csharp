d90b5900d702dd74c2e66a604f7356e49ff4c788
Andrew Hall (METAL) <andrha@microsoft.com>
2023-05-17 16:36:33 -0700

    Always use /// on URI schemes

--- src/lsptoolshost/uriConverter.ts
+++ src/lsptoolshost/uriConverter.ts
@@ -2,13 +2,18 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 import * as vscode from 'vscode';
 
 export class UriConverter {
     public static serialize(uri: vscode.Uri): string {
-        // Fix issue in System.Uri where file:///c%3A/file.txt is not a valid Windows path
-        return uri.toString(true);
+        if (uri.scheme === 'file') {
+            // Fix issue in System.Uri where file:///c%3A/file.txt is not a valid Windows path
+            return uri.toString(true);
+        } else {
+            let fileSchemUri = uri.with({scheme: 'file'});
+            let uriString = fileSchemUri.toString(true);
+            return uri.scheme + uriString.slice("file".length);
+        }
     }
 
     public static deserialize(value: string): vscode.Uri {
