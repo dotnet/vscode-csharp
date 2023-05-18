e1dc6a999473806b7492ce428178b395179fe23c
Andrew Hall (METAL) <andrha@microsoft.com>
2023-05-17 22:06:29 -0700

    Switch to check for exact scheme instead of 'file'

--- src/lsptoolshost/uriConverter.ts
+++ src/lsptoolshost/uriConverter.ts
@@ -4,13 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import { CSharpProjectedDocumentContentProvider } from '../razor/src/CSharp/CSharpProjectedDocumentContentProvider';
 
 export class UriConverter {
     public static serialize(uri: vscode.Uri): string {
-        if (uri.scheme === 'file') {
-            // Fix issue in System.Uri where file:///c%3A/file.txt is not a valid Windows path
-            return uri.toString(true);
-        } else {
+        if (uri.scheme === CSharpProjectedDocumentContentProvider.scheme) {
             // VSCode specifically handles file schemes different than others:
             // https://github.com/microsoft/vscode-uri/blob/b54811339bd748982d0e2697fa857a3fecc72522/src/uri.ts#L606
             // Since it's desirable that  URIs follow the same scheme across different OSs regardless of
@@ -19,6 +17,9 @@ export class UriConverter {
             let fileSchemUri = uri.with({scheme: 'file'});
             let uriString = fileSchemUri.toString(true);
             return uri.scheme + uriString.slice("file".length);
+        } else {
+            // Fix issue in System.Uri where file:///c%3A/file.txt is not a valid Windows path
+            return uri.toString(true);
         }
     }
 
