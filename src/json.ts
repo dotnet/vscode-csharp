24a3b33d93f77fcf191b52ba0893cbe7c334baff
David Barbet <dabarbet@microsoft.com>
2023-06-27 15:28:57 -0700

    Remove duplicate license header

--- src/json.ts
+++ src/json.ts
@@ -3,11 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 const enum CharCode {
     asterisk = 0x2a, // *
     backSlash = 0x5c, // \
