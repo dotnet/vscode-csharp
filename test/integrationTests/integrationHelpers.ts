01156898da1bf562f780f7aebc8427e1430df3c0
akhera99 <ankitakhera@microsoft.com>
2023-06-28 15:25:09 -0700

    wip

--- test/integrationTests/integrationHelpers.ts
+++ test/integrationTests/integrationHelpers.ts
@@ -31,9 +31,9 @@ export async function activateCSharpExtension(): Promise<ActivationResult> {
         await vscode.commands.executeCommand("workbench.action.reloadWindow");
     }
     
-    configuration.update('omnisharp.enableLspDriver', process.env.OMNISHARP_DRIVER === 'lsp' ? true : false);
+    configuration.update('omnisharp.enableLspDriver', process.env.OMNISHARP_DRIVER === 'lsp' ? true : false, vscode.ConfigurationTarget.WorkspaceFolder);
     if (process.env.OMNISHARP_LOCATION) {
-        configuration.update('path', process.env.OMNISHARP_LOCATION);
+        configuration.update('path', process.env.OMNISHARP_LOCATION, vscode.ConfigurationTarget.WorkspaceFolder);
     }
 
     const csharpExtension = vscode.extensions.getExtension<OmnisharpExtensionExports>("ms-dotnettools.csharp");
