1b0412f015882c61e80f2f931cf0ee73f9059e9e
Allison Chou <allichou@microsoft.com>
2023-07-19 14:53:08 -0700

    Separate Razor server and Typescript versions (O# and non-O#) (#5932)

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,7 @@ import { GlobalBrokeredServiceContainer } from '@microsoft/servicehub-framework'
 import { CSharpExtensionExports, OmnisharpExtensionExports } from './csharpExtensionExports';
 import { csharpDevkitExtensionId, getCSharpDevKit } from './utils/getCSharpDevKit';
 import { BlazorDebugConfigurationProvider } from './razor/src/blazorDebug/blazorDebugConfigurationProvider';
+import { RazorOmnisharpDownloader } from './razor/razorOmnisharpDownloader';
 
 export async function activate(
     context: vscode.ExtensionContext
@@ -126,7 +127,12 @@ export async function activate(
         // Roslyn starts up and registers Razor-specific didOpen/didClose/didChange commands and sends request to Razor
         //     for dynamic file info once project system is ready ->
         // Razor sends didOpen commands to Roslyn for generated docs and responds to request with dynamic file info
-        await activateRazorExtension(context, context.extension.extensionPath, eventStream);
+        await activateRazorExtension(
+            context,
+            context.extension.extensionPath,
+            eventStream,
+            /* useOmnisharpServer */ false
+        );
 
         context.subscriptions.push(optionProvider);
         context.subscriptions.push(
@@ -239,7 +245,24 @@ export async function activate(
         eventStream.subscribe(razorObserver.post);
 
         if (!razorOptions.razorDevMode) {
-            omnisharpRazorPromise = activateRazorExtension(context, context.extension.extensionPath, eventStream);
+            // Download Razor O# server
+            const razorOmnisharpDownloader = new RazorOmnisharpDownloader(
+                networkSettingsProvider,
+                eventStream,
+                context.extension.packageJSON,
+                platformInfo,
+                context.extension.extensionPath
+            );
+
+            await razorOmnisharpDownloader.DownloadAndInstallRazorOmnisharp(
+                context.extension.packageJSON.defaults.razorOmnisharp
+            );
+            omnisharpRazorPromise = activateRazorExtension(
+                context,
+                context.extension.extensionPath,
+                eventStream,
+                /* useOmnisharpServer */ true
+            );
         }
     }
 
