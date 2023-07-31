d088094c95ea1ef9d809cdac04b67ec2638e7bc0
Shen Chen <shech@microsoft.com>
2023-07-31 13:15:02 -0700

    Add rest of git command

--- tasks/localizationTasks.ts
+++ tasks/localizationTasks.ts
@@ -6,12 +6,11 @@
 import * as gulp from 'gulp';
 import * as process from 'node:process';
 import * as minimist from 'minimist';
-// import { createTokenAuth } from '@octokit/auth-token';
+import { createTokenAuth } from '@octokit/auth-token';
 import { spawnSync } from 'node:child_process';
 import * as fs from 'fs';
 import * as util from 'node:util';
 import { EOL } from 'node:os';
-import spawnNode from './spawnNode';
 
 type Options = {
     userName: string;
@@ -61,7 +60,6 @@ function getAllPossibleLocalizationFileNames(): string[] {
 
 gulp.task('publish localization content', async () => {
     const parsedArgs = minimist<Options>(process.argv.slice(2));
-    console.log(parsedArgs);
     await git_add(['-A']);
     const diffResults = await git_diff(['--name-only', 'HEAD']);
     if (diffResults.length == 0) {
@@ -75,50 +73,49 @@ gulp.task('publish localization content', async () => {
     }
 
     console.log('Authenticate PAT.');
-    // console.log('Authenticate PAT.');
-    // const pat = parsedArgs.pat ?? process.env['GitHubPAT'];
-    // if (!pat) {
-    //     throw 'No GitHub Pat found.';
-    // }
+    const pat = parsedArgs.pat ?? process.env['GitHubPAT'];
+    if (!pat) {
+        throw 'No GitHub Pat found.';
+    }
 
-    // const auth = createTokenAuth(pat);
-    // await auth();
-    // await git_config(['--local', 'user.name', parsedArgs.userName]);
-    // await git_config(['--local', 'user.email', parsedArgs.email]);
-    // await git_remote(['add', 'targetRepo', parsedArgs.targetRemoteRepo]);
-    // await git_fetch(['targetRepo']);
-    // await git_checkout(['-b', `localization/${parsedArgs.commitSha}`]);
-    // await git_commit(`Localization result of ${parsedArgs.commitSha}. `);
-    // await git_push(['-u', parsedArgs.targetRemoteRepo]);
+    const auth = createTokenAuth(pat);
+    await auth();
+    await git_config(['--local', 'user.name', parsedArgs.userName]);
+    await git_config(['--local', 'user.email', parsedArgs.email]);
+    await git_remote(['add', 'targetRepo', parsedArgs.targetRemoteRepo]);
+    await git_fetch(['targetRepo']);
+    await git_checkout(['-b', `localization/${parsedArgs.commitSha}`]);
+    await git_commit(['-m', `Localization result of ${parsedArgs.commitSha}.`]);
+    await git_push(['-u', parsedArgs.targetRemoteRepo]);
 });
 
-// async function git_config(options: string[]) {
-//     await git('config', options);
-// }
+async function git_config(options: string[]) {
+    await git('config', options);
+}
 
-// async function git_fetch(options: string[]): Promise<void> {
-//     await git('fetch', options);
-// }
+async function git_fetch(options: string[]): Promise<void> {
+    await git('fetch', options);
+}
 
-// async function git_remote(options: string[]): Promise<void> {
-//     await git('remote', options);
-// }
+async function git_remote(options: string[]): Promise<void> {
+    await git('remote', options);
+}
 
-// async function git_push(options: string[]): Promise<void> {
-//     await git('push', options);
-// }
+async function git_push(options: string[]): Promise<void> {
+    await git('push', options);
+}
 
-// async function git_checkout(options: string[]): Promise<void> {
-//     await git('checkout', options);
-// }
+async function git_checkout(options: string[]): Promise<void> {
+    await git('checkout', options);
+}
 
 async function git_add(filesOrDirectories: string[]): Promise<void> {
     await git('add', filesOrDirectories);
 }
 
-// async function git_commit(commitMessage: string): Promise<void> {
-//     await git('commit', ['-m', commitMessage]);
-// }
+async function git_commit(options: string[]): Promise<void> {
+    await git('commit', options);
+}
 
 async function git_diff(args?: string[]): Promise<string[]> {
     const result = await git('diff', args);
