9a33b7454c8ef029a86822a3feae9ad6f8c955b1
shech <shech@microsoft.com>
2023-08-09 15:33:22 -0700

    Get commit

--- tasks/RoslynTaggingTask.ts
+++ tasks/RoslynTaggingTask.ts
@@ -6,7 +6,7 @@
 import * as gulp from 'gulp';
 import * as xml2js from 'xml2js';
 import * as fs from 'fs';
-import * as axios from 'axios';
+import axios from 'axios';
 
 gulp.task('Tag release information to dotnet/roslyn', async () => {
     const packageJson = JSON.parse('../package.json');
@@ -28,18 +28,33 @@ gulp.task('Tag release information to dotnet/roslyn', async () => {
     // 1. Ask the PackageBaseAddress from nuget server index.
     // 2. Download .nuspec file. See https://learn.microsoft.com/en-us/nuget/api/package-base-address-resource#download-package-manifest-nuspec
     // 3. Find the commit from .nuspec file
+    const indexResponse = await axios.get(nugetServerIndex);
+    if (indexResponse.status != 200) {
+        LogError('Failed to get index file from nuget server');
+        return 1;
+    }
 
-    const languageServerPackageName = 'Microsoft.CodeAnalysis.LanguageServer';
+    console.log(`nuget index file: ${indexResponse.data}.`);
+    const resources = indexResponse.data['resources'] as any[];
+    // This is required to be implemented in nuget server.
+    const packageBaseAddress = resources.find((res, _) => res['@type'] === 'PackageBaseAddress/3.0.0');
+    const id = packageBaseAddress['@id'];
 
+    const lowerCaselanguageServerPackageName = 'Microsoft.CodeAnalysis.LanguageServer'.toLocaleLowerCase();
+    const nuspecGetUrl = `${id}/${lowerCaselanguageServerPackageName}/${roslynVersion}/${lowerCaselanguageServerPackageName}.nuspec`;
+    const nuspecResponse = await axios.get(nuspecGetUrl);
+    if (nuspecResponse.status != 200) {
+        LogError('Failed to get nuspec file from nuget server');
+        return 1;
+    }
+
+    console.log(`nuspect file: ${nugetConfig.data}.`);
+    const nuspecFile = await xml2js.parseStringPromise(nuspecResponse.data);
+    const repoUrl = nuspecFile.package.metadata.repository[0].$['url'];
+    const commitNumber = nuspecFile.package.metadata.repository[0].$['commit'];
+    console.log(`repoUrl is ${repoUrl}.`);
+    console.log(`commitNumber is ${commitNumber}.`);
 });
-
-interface nugetConfig {
-    configu1
-}
-
-async function HttpsGetAsync(url: string) : Promise<string> {
-    const request = https.get(url,);
-}
 
 function LogError(message: string): void {
     console.log(`##vso[task.logissue type=error]${message}`);
