514bc3ec3d131e6785fc3a82e128f4fadd49d51c
shech <shech@microsoft.com>
2023-08-09 15:10:38 -0700

    Add axios

--- tasks/RoslynTaggingTask.ts
+++ tasks/RoslynTaggingTask.ts
@@ -4,6 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as gulp from 'gulp';
+import * as xml2js from 'xml2js';
+import * as fs from 'fs';
+import * as axios from 'axios';
 
 gulp.task('Tag release information to dotnet/roslyn', async () => {
     const packageJson = JSON.parse('../package.json');
@@ -13,8 +16,30 @@ gulp.task('Tag release information to dotnet/roslyn', async () => {
         return 1;
     }
 
-    // const languageServerPackageName = 'Microsoft.CodeAnalysis.LanguageServer';
+    const nugetFile = fs.readFileSync('../server/NuGet.config');
+    const nugetConfig = await xml2js.parseStringPromise(nugetFile);
+    const nugetServerIndex = nugetConfig.configuration.packageSources.add[0].$['msft_consumption'];
+    if (!nugetServerIndex) {
+        LogError("Can't find nuget server index.");
+        return 1;
+    }
+
+    // Find the matching commit from nuget server
+    // 1. Ask the PackageBaseAddress from nuget server index.
+    // 2. Download .nuspec file. See https://learn.microsoft.com/en-us/nuget/api/package-base-address-resource#download-package-manifest-nuspec
+    // 3. Find the commit from .nuspec file
+
+    const languageServerPackageName = 'Microsoft.CodeAnalysis.LanguageServer';
+
 });
+
+interface nugetConfig {
+    configu1
+}
+
+async function HttpsGetAsync(url: string) : Promise<string> {
+    const request = https.get(url,);
+}
 
 function LogError(message: string): void {
     console.log(`##vso[task.logissue type=error]${message}`);
