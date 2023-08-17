b1579742b5e2c12ddf1d76716b81f9d2439b2577
Ankita Khera <40616383+akhera99@users.noreply.github.com>
2023-08-16 21:52:05 -0700

    Integration Tests - Change directory name of omnisharp tests (#6146)
    * rename progress
    
    * more updates
    
    * change lsptoolshosttest to test
    
    * fix ci
    
    * whoops
    
    * fix tslint

--- omnisharptest/testRunner.ts
+++ omnisharptest/testRunner.ts
@@ -6,15 +6,32 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob-promise';
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+import { SubscribeToAllLoggers } from '../src/logger';
 import * as tty from 'tty';
 
 // Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
-// Since we are not running in a tty environment, we just implement the method statically
+// Since we are not running in a tty environment, we just implementt he method statically
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 tty.getWindowSize = function () {
     return [80, 75];
 };
+
+function setupLogging() {
+    if (process.env.CODE_EXTENSIONS_PATH && process.env.OSVC_SUITE) {
+        const logDirPath = path.join(process.env.CODE_EXTENSIONS_PATH, './.logs');
+
+        if (!fs.existsSync(logDirPath)) {
+            fs.mkdirSync(logDirPath);
+        }
+
+        const logFilePath = path.join(logDirPath, `${process.env.OSVC_SUITE}.log`);
+
+        SubscribeToAllLoggers((message) => fs.appendFileSync(logFilePath, message));
+    }
+}
 
 export async function run(testsRoot: string, options?: Mocha.MochaOptions) {
     options ??= {
@@ -23,7 +40,14 @@ export async function run(testsRoot: string, options?: Mocha.MochaOptions) {
         retries: 2,
     };
 
+    const configuration = vscode.workspace.getConfiguration();
+    const enableLspDriver = configuration.get<boolean | undefined>('omnisharp.enableLspDriver', undefined);
+    configuration.update('omnisharp.enableLspDriver', process.env.OMNISHARP_DRIVER === 'lsp' ? true : false);
+
     const mocha = new Mocha(options);
+
+    setupLogging();
+
     // Glob test files
     const files = await glob('**/**.test.js', { cwd: testsRoot });
 
@@ -36,5 +60,7 @@ export async function run(testsRoot: string, options?: Mocha.MochaOptions) {
         if (failures > 0) {
             throw new Error(`${failures} tests failed.`);
         }
+
+        configuration.update('omnisharp.enableLspDriver', enableLspDriver);
     });
 }
