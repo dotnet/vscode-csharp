3f9fbc664872e9939308f28b0a9b8417742e0f53
Allison Chou <allichou@microsoft.com>
2023-09-05 18:58:12 -0700

    Update test

--- test/razorTests/jest.config.ts
+++ test/razorTests/jest.config.ts
@@ -17,9 +17,7 @@ const razorConfig: Config = {
     // the TS version from the __mocks__ directory and the compiled js version from the out directory.
     modulePathIgnorePatterns: ['out'],
     // Specify jest to only run tests in jest folders.
-    // We also have to include the __mocks__ folder.  That folder must be next to node_modules so we can't move it,
-    // but if we specify roots, jest won't automatically pick it up.  So we have to specify it here.
-    roots: ['<rootDir>', '<rootDir>/../../__mocks__'],
+    roots: ['<rootDir>'],
 };
 
 export default razorConfig;
