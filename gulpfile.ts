918872fd1fecb716f36b4d186707fcf50a2e30ee
Andrew Wang <waan@microsoft.com>
2023-09-14 14:03:17 -0700

    Validate package.nls.json for \u200b (#6287)
    * Validate package.nls.json for \u200b
    
    This PR adds in a check to see if \u200b is included in the loc strings
    that are used to break hyperlink syntax in descriptions.
    
    Added a new gulp task that will quickly resolve these issues.
    
    Also moved the debugger gulp tasks to tasks\debuggerTasks.ts.

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,25 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as gulp from 'gulp';
-import * as optionsSchemaGenerator from './src/tools/generateOptionsSchema';
-import * as packageDependencyUpdater from './src/tools/updatePackageDependencies';
-
 require('./tasks/testTasks');
 require('./tasks/offlinePackagingTasks');
 require('./tasks/backcompatTasks');
 require('./tasks/localizationTasks');
 require('./tasks/createTagsTasks');
-
-// Disable warning about wanting an async function
-// tslint:disable-next-line
-gulp.task('generateOptionsSchema', async (): Promise<void> => {
-    optionsSchemaGenerator.GenerateOptionsSchema();
-    return Promise.resolve();
-});
-
-// Disable warning about wanting an async function
-// tslint:disable-next-line
-gulp.task('updatePackageDependencies', async (): Promise<void> => {
-    return packageDependencyUpdater.updatePackageDependencies();
-});
+require('./tasks/debuggerTasks');
