089faf86038f7c655ade0a75d697923f8fe5273f
Fred Silberberg <frsilb@microsoft.com>
2023-11-09 15:41:30 -0800

    Rearrange the settings sections into actual categories (#6652)
    This puts settings into discrete categories ("Project", "Text Editor", "Debugger", "LSP Server", and "OmniSharp") so that our settings can be reasonably navigated by a human, rather than being a giant blob that cannot be properly viewed.

--- test/unitTests/migrateOptions.test.ts
+++ test/unitTests/migrateOptions.test.ts
@@ -9,8 +9,14 @@ import * as jestLib from '@jest/globals';
 
 jestLib.describe('Migrate configuration should in package.json', () => {
     const packageJson = JSON.parse(readFileSync('package.json').toString());
-    const properties = packageJson.contributes.configuration[1].properties;
-    const configurations = Object.keys(properties);
+    const configuration = packageJson.contributes.configuration;
+    // Read the "Project", "Text Editor", "Debugger", "LSP Server" sections of the package.json
+    const configurations = [
+        ...Object.keys(configuration[0].properties),
+        ...Object.keys(configuration[1].properties),
+        ...Object.keys(configuration[2].properties),
+        ...Object.keys(configuration[3].properties),
+    ];
 
     migrateOptions.forEach((data) => {
         jestLib.test(`Should have ${data.roslynOption} in package.json`, () => {
