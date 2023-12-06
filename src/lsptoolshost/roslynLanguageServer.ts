e59cfbb3dd58eba76405a9dfecc3deccf8e9f294
David Barbet <dabarbet@microsoft.com>
2023-12-06 11:22:13 -0800

    Read extension paths option

--- src/lsptoolshost/roslynLanguageServer.ts
+++ src/lsptoolshost/roslynLanguageServer.ts
@@ -902,7 +902,7 @@ export async function activateRoslynLanguageServer(
     return languageServer;
 
     function scanExtensionPlugins(): string[] {
-        return vscode.extensions.all.flatMap((extension) => {
+        const extensionsFromPackageJson = vscode.extensions.all.flatMap((extension) => {
             let loadPaths = extension.packageJSON.contributes?.['csharpExtensionLoadPaths'];
             if (loadPaths === undefined || loadPaths === null) {
                 _traceChannel.appendLine(`Extension ${extension.id} does not contribute csharpExtensionLoadPaths`);
@@ -920,6 +920,8 @@ export async function activateRoslynLanguageServer(
             _traceChannel.appendLine(`Extension ${extension.id} contributes csharpExtensionLoadPaths: ${loadPaths}`);
             return loadPaths;
         });
+        const extensionsFromOptions = languageServerOptions.extensionsPaths ?? [];
+        return extensionsFromPackageJson.concat(extensionsFromOptions);
     }
 }
 
