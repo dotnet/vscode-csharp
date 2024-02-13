b7b429dfeefdd17c3b0dfc2a5dff2a80fc2f3744
Jordi Ramos <jordi1215.jrc@gmail.com>
2024-02-13 13:36:04 -0800

    added grammar support for new directives
    - added grammar support for rendermode, preservewhitespace and typeparam in tmLanguage.yml
    - populated the corresponding json file with the npm run compile:razorTextMate command
    - added the corresponding tests in Microsoft.AspNetCore.Razor.VSCode.Grammar.Test

---
+++ test/razorTests/Microsoft.AspNetCore.Razor.VSCode.Grammar.Test/tests/rendermodeDirective.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { describe, it } from '@jest/globals';
+import { assertMatchesSnapshot } from './infrastructure/testUtilities';
+
+// See GrammarTests.test.ts for details on exporting this test suite instead of running in place.
+
+export function RunRendermodeDirectiveSuite() {
+    describe('@rendermode directive', () => {
+        it('No mode', async () => {
+            await assertMatchesSnapshot('@rendermode');
+        });
+
+        it('No mode spaced', async () => {
+            await assertMatchesSnapshot('@rendermode              ');
+        });
+
+        it('Incomplete mode, generic', async () => {
+            await assertMatchesSnapshot('@rendermode InteractiveWebAssemb');
+        });
+
+        it('Mode provided', async () => {
+            await assertMatchesSnapshot('@rendermode InteractiveServer');
+        });
+
+        it('Mode provided spaced', async () => {
+            await assertMatchesSnapshot('@rendermode              InteractiveAuto         ');
+        });
+    });
+}
