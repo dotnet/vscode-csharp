134417a66bf7fa7b71c1c71c63555c1f4ef427d4
vsol <45532845+blipk@users.noreply.github.com>
2024-08-30 10:58:03 +1000

    update patches

--- _patches/patches.sh
+++ _patches/patches.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
-# set -x
+set -x
 set -e
 
 # Patches from here https://github.com/dotnet/vscode-csharp/compare/main...muhammadsammy:free-vscode-csharp:master#diff-0ccaa77cc937eeac924d069abe67a8510757b97f326950c258d0e74a61a461d0R290
-
-
-# Patch 1: Remove specific lines from .eslintrc.js
-sed -i '/"header\/header": \[ 2, "block", \[/,/^\s*\]\s*\]\s*$/d' .eslintrc.js
-
 
 
 # Remove/update licences
 rm RuntimeLicenses/dependencies/OpenDebugAD7-License.txt || true
 yes | /bin/cp -rf _patches/RuntimeLicences-license.txt RuntimeLicenses/license.txt
 sed -i "s/Copyright (c) .NET Foundation and Contributors/Copyright (c) Blipk A.D. and Contributors for Current Work and Modifications in This Fork,\nCopyright (c) Muhammad Sammy and Contributors for Current Work and Modifications in Their Fork.\nOriginal Work Copyright (c) .NET Foundation and Contributors/" LICENSE.txt
+sed -i '/"header\/header": \[ 2, "block", \[/,/^\s*\]\s*\]\s*$/d' .eslintrc.js
 
-
-
-#TODO: Patch image
+# Patch image
+yes | /bin/cp -rf _patches/csharpIcon.png images/csharpIcon.png
 
 # Replace extension name
 EXTENSION_NAME="blipk-vscodium.csharp"
-find . -type f -exec sed -i "s/blipk-vscodium.csharp/$EXTENSION_NAME/g" {} +
+find . -type f -exec sed -i "s/ms-dotnettools.csharp/$EXTENSION_NAME/g" {} +
 
 # Patch src/coreclrDebug/activate.ts
 sed -i "s/if (!executable) {/const pipeTransport = _session.configuration.pipeTransport;\n        if (!executable || typeof pipeTransport === 'object') {/" src/coreclrDebug/activate.ts
@@ -71,11 +66,17 @@ rm temp_replacement.sed
 # Patch src/lsptoolshost/roslynLanguageServer.ts
 sed -i "/import TelemetryReporter from '@vscode\/extension-telemetry';/d" src/lsptoolshost/roslynLanguageServer.ts
 sed -i "/private _telemetryReporter: TelemetryReporter,/d" src/lsptoolshost/roslynLanguageServer.ts
-
-sed -i "s/const server = new RoslynLanguageServer(client, platformInfo, context, telemetryReporter, languageServerEvents);/const server = new RoslynLanguageServer(client, platformInfo, context, languageServerEvents);/" src/lsptoolshost/roslynLanguageServer.ts
-sed -i "/telemetryReporter: TelemetryReporter,/d" src/lsptoolshost/roslynLanguageServer.ts
 sed -i "/            reportProjectConfigurationEvent(/,/);/c\            reportProjectConfigurationEvent(params, this._platformInfo, dotnetInfo, this._solutionFile?.fsPath, true);" src/lsptoolshost/roslynLanguageServer.ts
 sed -i "/return await this.startServer(/,/);/c\            return await this.startServer(platformInfo, hostExecutableResolver, context, additionalExtensionPaths);" src/lsptoolshost/roslynLanguageServer.ts
+sed -i "/telemetryReporter: TelemetryReporter,/d" src/lsptoolshost/roslynLanguageServer.ts
+
+pattern="\/\/ shouldn't this arg only be set if it's running with CSDevKit\?\s+args\.push\('--telemetryLevel', telemetryReporter\.telemetryLevel\);"
+python _patches/replacer.py "src/lsptoolshost/roslynLanguageServer.ts" "$pattern" ""
+
+sed -i "s/const server = new RoslynLanguageServer(client, platformInfo, context, telemetryReporter, languageServerEvents);/const server = new RoslynLanguageServer(client, platformInfo, context, languageServerEvents);/" src/lsptoolshost/roslynLanguageServer.ts
+
+sed -i "/reporter: TelemetryReporter,/d" src/lsptoolshost/roslynLanguageServer.ts
+sed -i "/        reporter,/d" src/lsptoolshost/roslynLanguageServer.ts
 
 
 
@@ -84,6 +85,7 @@ sed -i "/import { TelemetryObserver } from '.\/observers\/telemetryObserver';/d"
 sed -i "/import TelemetryReporter from '@vscode\/extension-telemetry';/d" src/main.ts
 sed -i "/const aiKey = context.extension.packageJSON.contributes.debuggers\[0\].aiKey;/d" src/main.ts
 sed -i "/const reporter = new TelemetryReporter(aiKey);/d" src/main.ts
+sed -i "/    \/\/ ensure it gets properly disposed. Upon disposal the events will be flushed./d" src/main.ts
 sed -i "/context.subscriptions.push(reporter);/d" src/main.ts
 sed -i "/const telemetryObserver = new TelemetryObserver(platformInfo, () => reporter, useModernNetOption);/d" src/main.ts
 sed -i "/eventStream.subscribe(telemetryObserver.post);/d" src/main.ts
@@ -189,6 +191,13 @@ sed -i "/let telemetryExtensionDllPath = '';/d" src/razor/src/extension.ts
 
 pattern="await setupDevKitEnvironment(.|\s)*?razorComponentPaths\[0\];(.|\s)*?}(.|\s)*?}"
 python _patches/replacer.py "src/razor/src/extension.ts" "$pattern" "await setupDevKitEnvironment(env, csharpDevkitExtension, logger);"
+
+pattern="vscodeTelemetryReporter,
+            telemetryExtensionDllPath,
+            dotnetInfo.env,"
+replacement="csharpDevkitExtension !== undefined,
+            env,"
+python _patches/replacer.py "src/razor/src/extension.ts" "$pattern" "$replacement"
 
 
 # Patch src/razor/src/razorLanguageServerClient.ts
