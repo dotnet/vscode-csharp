150c3c985f9efc860cac950e9581b2fbce792da6
Andrew Hall (METAL) <andrha@microsoft.com>
2024-12-05 16:52:27 -0800

    Add html test. Fix test file

--- test/razor/razorIntegrationTests/completion.integration.test.ts
+++ test/razor/razorIntegrationTests/completion.integration.test.ts
@@ -33,7 +33,7 @@ integrationHelpers.describeIfWindows(`Razor Completion ${testAssetWorkspace.desc
 
         const insertPosition = new vscode.Position(4, 4);
         await insertText(insertPosition, '<te');
-        const completionList = await getCompletionsAsync(new vscode.Position(4, 7), 'e');
+        const completionList = await getCompletionsAsync(new vscode.Position(4, 7), 'e', undefined);
         expect(completionList.items.length).toBeGreaterThan(0);
         const textTagCompletionItem = completionList.items.find((item) => item.label === 'text');
 
@@ -46,9 +46,38 @@ integrationHelpers.describeIfWindows(`Razor Completion ${testAssetWorkspace.desc
         expect(textTagCompletionItem!.insertText).toBe('text');
     });
 
+    test('Div Tag', async () => {
+        if (!integrationHelpers.isRazorWorkspace(vscode.workspace)) {
+            return;
+        }
+
+        const insertPosition = new vscode.Position(6, 0);
+        await insertText(insertPosition, '<di');
+        const completionList = await getCompletionsAsync(new vscode.Position(6, 3), undefined, 10);
+        expect(completionList.items.length).toBeGreaterThan(0);
+        const divTagCompletionItem = completionList.items.find((item) => item.label === 'div');
+
+        if (!divTagCompletionItem) {
+            throw new Error(completionList.items.reduce((acc, item) => acc + item.label + '\n', ''));
+        }
+
+        expect(divTagCompletionItem).toBeDefined();
+
+        // Reader, you may be wondering why the kind is a Property. To that I say: I don't know.
+        // If you find out please add a detailed explanation. Thank you in advance.
+        expect(divTagCompletionItem!.kind).toEqual(vscode.CompletionItemKind.Property);
+        expect(divTagCompletionItem!.insertText).toBe('div');
+
+        const documentation = divTagCompletionItem!.documentation as vscode.MarkdownString;
+        expect(documentation.value).toBe(
+            'The div element has no special meaning at all. It represents its children. It can be used with the class, lang, and title attributes to mark up semantics common to a group of consecutive elements.\n\n[MDN Reference](https://developer.mozilla.org/docs/Web/HTML/Element/div)'
+        );
+    });
+
     async function getCompletionsAsync(
         position: vscode.Position,
-        triggerCharacter: string | undefined
+        triggerCharacter: string | undefined,
+        resolveCount: number | undefined
     ): Promise<vscode.CompletionList> {
         const activeEditor = vscode.window.activeTextEditor;
         if (!activeEditor) {
@@ -59,7 +88,8 @@ integrationHelpers.describeIfWindows(`Razor Completion ${testAssetWorkspace.desc
             'vscode.executeCompletionItemProvider',
             activeEditor.document.uri,
             position,
-            triggerCharacter
+            triggerCharacter,
+            resolveCount
         );
     }
 
