0fe35471146f9daf2f3cf1fe586189a34496fdd7
David Wengier <git@wengier.com>
2025-04-25 08:41:39 +1000

    Add document color endpoints

--- src/lsptoolshost/razor/razorEndpoints.ts
+++ src/lsptoolshost/razor/razorEndpoints.ts
@@ -5,13 +5,25 @@
 
 import { RoslynLanguageServer } from '../server/roslynLanguageServer';
 import * as vscode from 'vscode';
-import { LogMessageParams, NotificationType, RequestType } from 'vscode-languageclient';
+import {
+    ColorInformation,
+    ColorPresentationParams,
+    ColorPresentationRequest,
+    DocumentColorParams,
+    DocumentColorRequest,
+    LogMessageParams,
+    NotificationType,
+    RequestType,
+} from 'vscode-languageclient';
 import { RazorLogger } from '../../razor/src/razorLogger';
 import { HtmlUpdateParameters } from './htmlUpdateParameters';
 import { UriConverter } from '../utils/uriConverter';
 import { PlatformInformation } from '../../shared/platform';
 import { HtmlDocumentManager } from './htmlDocumentManager';
+import { DocumentColorHandler } from '../../razor/src/documentColor/documentColorHandler';
 import { razorOptions } from '../../shared/options';
+import { ColorPresentationHandler } from '../../razor/src/colorPresentation/colorPresentationHandler';
+import { ColorPresentation } from 'vscode-html-languageservice';
 
 export function registerRazorEndpoints(
     context: vscode.ExtensionContext,
@@ -35,6 +47,23 @@ export function registerRazorEndpoints(
         const uri = UriConverter.deserialize(params.textDocument.uri);
         await documentManager.updateDocumentText(uri, params.text);
     });
+
+    registerRequestHandler<DocumentColorParams, ColorInformation[]>(DocumentColorRequest.method, async (params) => {
+        const uri = UriConverter.deserialize(params.textDocument.uri);
+        const document = await documentManager.getDocument(uri);
+
+        return await DocumentColorHandler.doDocumentColorRequest(document.uri);
+    });
+
+    registerRequestHandler<ColorPresentationParams, ColorPresentation[]>(
+        ColorPresentationRequest.method,
+        async (params) => {
+            const uri = UriConverter.deserialize(params.textDocument.uri);
+            const document = await documentManager.getDocument(uri);
+
+            return await ColorPresentationHandler.doColorPresentationRequest(document.uri, params);
+        }
+    );
 
     // Helper method that registers a request handler, and logs errors to the Razor logger.
     function registerRequestHandler<Params, Result>(method: string, invocation: (params: Params) => Promise<Result>) {
