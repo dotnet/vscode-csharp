e61846fb778d7367db17c55785ca3060572fbee4
Joey Robichaud <joseph.robichaud@microsoft.com>
2026-06-09 09:28:50 -0700

    Remove trailing spaces to make prettier happy

--- tasks/tests/testHelpers.ts
+++ tasks/tests/testHelpers.ts
@@ -87,13 +87,13 @@ export async function runJestIntegrationTest(
     suiteName: string,
     env: NodeJS.ProcessEnv = {},
     testFile: string | undefined = undefined
-) { 
+) {
     const logName = testFile ? `${suiteName}_${path.basename(testFile)}` : suiteName;
 
     // Set VSCode to produce logs in a unique directory for this test run.
     //
     // Since VSCode also uses this folder for creating sock files for IPC
-    // and those handles are limited to 103 characters, we will trim the 
+    // and those handles are limited to 103 characters, we will trim the
     // path to 85 characters.
     const userDataDir = path.join(outPath, 'userData', logName).substring(0, 85);
 
